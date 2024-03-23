a6b3e7da222d3523d7e3b3baf78336137921deae
DominicGorny <dominic.gorny11@gmail.com>
2024-03-23 15:02:40 +1300

    Added automated unit tests for uniqueness validation in emails. Testing the input validator methods.

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/InputValidatorTest.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/InputValidatorTest.java
@@ -1,16 +1,53 @@
 package nz.ac.canterbury.seng302.gardenersgrove;
-import jakarta.persistence.criteria.CriteriaBuilder;
+import nz.ac.canterbury.seng302.gardenersgrove.service.UserService;
 import nz.ac.canterbury.seng302.gardenersgrove.validation.InputValidator.InputValidator;
 import nz.ac.canterbury.seng302.gardenersgrove.validation.InputValidator.ValidationResult;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class InputValidatorTest {
 
+    /**
+     * Setup a mock userService class for testing that email uniqueness validation works
+     */
+    private static UserService userServiceMock;
+
+    @BeforeAll
+    public static void setup() {
+        userServiceMock = Mockito.mock(UserService.class);
+        InputValidator testValidator = new InputValidator();
+        testValidator.UserService(userServiceMock);
+        Mockito.when(userServiceMock.emailInUse(Mockito.any())).thenReturn(false);
+    }
+
+    @BeforeEach
+    public void resetInterface()
+    {
+        Mockito.when(userServiceMock.emailInUse(Mockito.any())).thenReturn(false);
+    }
+
+    @Test
+    public void InputValidator_validateUniqueEmail_uniqueEmail_return_OK()
+    {
+        Mockito.when(userServiceMock.emailInUse(Mockito.any())).thenReturn(false);
+        assertEquals(ValidationResult.OK,InputValidator.validateUniqueEmail("jondoe@gmail.com"));
+        Mockito.verify(userServiceMock,Mockito.atLeastOnce()).emailInUse(Mockito.any());
+    }
+
+    @Test
+    public void InputValidator_validateUniqueEmail_duplicatedEmail_return_NON_UNIQUE_EMAIL()
+    {
+        Mockito.when(userServiceMock.emailInUse(Mockito.any())).thenReturn(true);
+        assertEquals(ValidationResult.NON_UNIQUE_EMAIL,InputValidator.validateUniqueEmail("jondoe@gmail.com"));
+        Mockito.verify(userServiceMock,Mockito.atLeastOnce()).emailInUse(Mockito.any());
+    }
 
     @ParameterizedTest
     @ValueSource(strings = {"Hello", "Hello World","àäèéëïĳöü","áêéèëïíîôóúû","êôúû","ÆØÅæøå","ÄÖäö",
