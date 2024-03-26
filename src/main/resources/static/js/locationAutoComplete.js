a2981802a7d169b4d5ebed1a115bc80809b2e3c1
Stephen Huang <shu122@uclive.ac.nz>
2024-03-26 16:36:59 +1300

    Implement Google Places API and added its autocomplete to garden form location fields

---
+++ src/main/resources/static/js/locationAutoComplete.js
@@ -0,0 +1,60 @@
+
+let autocomplete;
+
+function initAutocomplete() {
+    // Create the autocomplete object, restricting the search to geographical
+    // location types.
+    autocomplete = new google.maps.places.Autocomplete(
+        /** @type {!HTMLInputElement} */(document.getElementById('gardenLocation')),
+        {types: ['geocode', 'establishment']});
+
+    // When the user selects an address from the dropdown, populate the address
+    // fields in the form.
+    autocomplete.addListener('place_changed', fillInAddress);
+}
+function fillInAddress() {
+    const place = autocomplete.getPlace();
+    let addr = "";
+
+    let streetNumber;
+    let route;
+    let suburb;
+    let city;
+    let postcode;
+    let country;
+
+    for (const component of place.address_components) {
+        const componentType = component.types[0];
+
+        switch (componentType) {
+            case "street_number":
+                streetNumber = component.long_name;
+                break;
+            case "route":
+                route = component.long_name;
+                break;
+            case "sublocality":
+            case "sublocality_level_1":
+            case "neighborhood":
+                suburb = component.long_name;
+                break;
+            case "locality":
+                city = component.long_name;
+                break;
+            case "postal_code":
+                postcode = component.long_name;
+                break;
+            case "country":
+                country = component.long_name;
+                break;
+        }
+    }
+
+    addr = `${streetNumber} ${route}, ${suburb}, ${city} ${postcode}, ${country}`;
+
+    document.getElementById('gardenLocation').value = addr;
+
+}
+
+
+window.initAutocomplete = initAutocomplete;
