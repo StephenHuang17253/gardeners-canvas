b6abd6089ef36c2474ae6872bc37c3193671e443
Lachlan Stewart <lst117@uclive.ac.nz>
2024-03-30 17:28:30 +1300

    tried code input hmtl

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/ProfileController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/ProfileController.java
@@ -370,4 +370,11 @@ public class ProfileController {
         return "redirect:/profile";
     }
 
+
+    @GetMapping("/profile/validate")
+    public String validateProfile(Model model) {
+        logger.info("GET /profile/validate");
+
+        return "validateProfile";
+    }
 }
