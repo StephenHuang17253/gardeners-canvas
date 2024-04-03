136d0ff2b27c3e99239dc7ec0ed14ebdb932655e
DominicGorny <dominic.gorny11@gmail.com>
2024-04-03 21:11:03 +1300

    Testing: Attempted to add tests for posting user edits on userProfileController but got stuck. For some reason, the way users are authenticated for the test does not seem to work for post Requests (only get requests). (Expect last two tests in ProfileControllerTest.java to fail for this reason)

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/controllerTests/Integration/ProfileControllerTest.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/controllerTests/Integration/ProfileControllerTest.java
@@ -2,6 +2,7 @@ package nz.ac.canterbury.seng302.gardenersgrove.controllerTests.Integration;
 
 import nz.ac.canterbury.seng302.gardenersgrove.controller.ProfileController;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.User;
+import nz.ac.canterbury.seng302.gardenersgrove.service.FileService;
 import nz.ac.canterbury.seng302.gardenersgrove.service.UserService;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,6 +19,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.RequestPostProcessor;
+import org.springframework.web.multipart.MultipartFile;
 
 
 import java.time.LocalDate;
@@ -27,6 +29,7 @@ import java.util.Locale;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -39,6 +42,9 @@ public class ProfileControllerTest {
 
     @MockBean
     private UserService userServiceMock;
+
+    @MockBean
+    private FileService fileServiceMock;
 
 
     @InjectMocks
@@ -57,6 +63,7 @@ public class ProfileControllerTest {
             return mockRequest;
         };
     }
+
     @BeforeEach
     public void setup() {
         //Setup an mock user
@@ -70,6 +77,12 @@ public class ProfileControllerTest {
         Mockito.when(userServiceMock.getUserByEmailAndPassword("sally@email.com", "Trial@10!")).thenReturn(mockUser);
         Mockito.when(userServiceMock.getUserByEmailAndPassword("sally@email.com", mockUser.getEncodedPassword())).thenReturn(mockUser);
 
+    }
+
+    @AfterAll
+    public static void cleanup() {
+        // Clear the context tests
+        SecurityContextHolder.clearContext();
     }
 
     @Test
@@ -86,8 +99,17 @@ public class ProfileControllerTest {
     }
 
     @Test
+    public void getProfilePage_notAuthenticated_Forbidden() throws Exception
+    {
+        mockMvc.perform(get("/profile"))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
     public void getProfilePage_LoggedIn_FilledPage() throws Exception
     {
+        // Todo: Fix date format on site such that this test passes
+        // test is currently correct but fails because format on site is 2001-12-12 (YYYYY-MM-DD)
         mockMvc.perform(get("/profile").with(userAuthenticated()))
                 .andExpect(status().isOk())
                 .andExpect(model().attribute("userName","Sally Doe"))
@@ -95,9 +117,22 @@ public class ProfileControllerTest {
                 .andExpect(model().attribute("emailAddress","sally@email.com"));
     }
 
-    @AfterAll
-    public static void cleanup() {
-    // Clear the context tests
-        SecurityContextHolder.clearContext();
+    // Todo Currently the two test below here do not pass because the server response is forbidden
+    @Test
+    public void postProfilePage_LoggedIn_FilledPage() throws Exception
+    {
+        MultipartFile newProfilePhotoMock = Mockito.mock(MultipartFile.class);
+
+        mockMvc.perform(post("/profile").with(userAuthenticated()).flashAttr("profilePictureInput", newProfilePhotoMock))
+                .andExpect(status().is3xxRedirection());
     }
+
+    @Test
+    public void postProfileEditPage_Empty_Ok() throws Exception
+    {
+        mockMvc.perform(post("/profile/edit").with(userAuthenticated()))
+                .andExpect(status().is3xxRedirection());
+    }
+
+
 }
