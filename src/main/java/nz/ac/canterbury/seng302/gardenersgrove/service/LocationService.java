f66bf95672a521d385ec0a2fa196c90657a84d51
Stephen Huang <shu122@uclive.ac.nz>
2024-04-09 19:57:23 +1200

    Revert "Merge remote-tracking branch 'origin/garden-location-api-(locationIQ)' into..."
    This reverts commit 47ef93f6a0d3d79965090f01d4b561a9adaefb63

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/service/LocationService.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/service/LocationService.java
@@ -1,9 +1,6 @@
 package nz.ac.canterbury.seng302.gardenersgrove.service;
 
-import io.github.cdimascio.dotenv.Dotenv;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -13,12 +10,22 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+/**
+ * Service class for obtaining location autocomplete suggestions using the LocationIQ API.
+ */
 @Service
 public class LocationService {
 
-    @Value("${location.iq.access.token}")
+    @Value("${locationiq.access.token}")
     private String locationIqAccessToken;
 
+    /**
+     * Retrieves location suggestions based on provided query string.
+     * @param query The string for which autocomplete suggestions are requested.
+     * @return A JSON string containing location suggestions
+     * @throws IOException If an error occurs while making request
+     * @throws InterruptedException If request is interrupted
+     */
     public String getLocationSuggestions(String query) throws IOException, InterruptedException {
 ;
         String encodedQuery = URLEncoder.encode(query, "UTF-8");
@@ -29,7 +36,8 @@ public class LocationService {
                 .method("GET", HttpRequest.BodyPublishers.noBody())
                 .build();
 
-        System.out.println("https://api.locationiq.com/v1/autocomplete?q=" + query + "&key=" + locationIqAccessToken);
+        System.out.println("https://api.locationiq.com/v1/autocomplete?q=" + encodedQuery + "&key=" + locationIqAccessToken);
+        System.out.println("Call to API with query = '" + query + "'");
         HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
         // System.out.println(response.body());
         return response.body();
