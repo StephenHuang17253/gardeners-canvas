f5f7f57aebcf0d607c6c669432cc25bcc3e33f6e
Finlay Widdowson <fwi28@uclive.ac.nz>
2024-04-25 15:15:06 +1200

    Added password checking for the reset password, displays errors based on password strength and matching.

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/ResetPasswordController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/ResetPasswordController.java
@@ -1,12 +1,18 @@
 package nz.ac.canterbury.seng302.gardenersgrove.controller;
 
 import nz.ac.canterbury.seng302.gardenersgrove.service.UserService;
+import nz.ac.canterbury.seng302.gardenersgrove.validation.InputValidator.InputValidator;
+import nz.ac.canterbury.seng302.gardenersgrove.validation.InputValidator.ValidationResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Objects;
 
 
 @Controller
@@ -32,7 +38,15 @@ public class ResetPasswordController {
 
     }
     @PostMapping("/reset-password")
-    public String passwordChecker() {
+    public String passwordChecker(@RequestParam("password") String password, @RequestParam("retypePassword") String retypePassword, Model model) {
+        ValidationResult passwordValidation = InputValidator.validatePassword(password);
+
+        ValidationResult rePasswordValidation = InputValidator.validatePassword(password);
+        if (!passwordValidation.valid()) {
+            model.addAttribute("passwordError", passwordValidation);
+        } else if (!Objects.equals(password, retypePassword)) {
+            model.addAttribute("passwordError", "The passwords do not match");
+        }
         return "resetPasswordForm";
     }
 
