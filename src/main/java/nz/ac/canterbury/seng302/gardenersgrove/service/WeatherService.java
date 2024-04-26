60537a9e5190f7a42af88793f7b8ea85a4348e25
Emma Davis <eda60@uclive.ac.nz>
2024-04-26 14:23:50 +1200

    Implemented getting the data from the api in form of JSON object.

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/service/WeatherService.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/service/WeatherService.java
@@ -1,5 +1,7 @@
 package nz.ac.canterbury.seng302.gardenersgrove.service;
 
+
+import org.json.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,25 +14,41 @@ public class WeatherService {
 
     Logger logger = LoggerFactory.getLogger(WeatherService.class);
 
-    public void getWeather(String latitude, String longitude) {
-        String url = "https://api.open-meteo.com/v1/forecast?latitude="
-                + latitude
-                + "&longitude=" + longitude
-                + "&current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,rain,showers,wind_speed_10m";
+    JSONObject current;
+    double currentTemp;
+    int currentHumidity;
+    int currentWeather;
 
-        HttpClient client = HttpClient.newHttpClient();
+    JSONObject hourly;
+
+
+    public void getWeather(String gardenLatitude, String gardenLongitude) {
+        String url = "https://api.open-meteo.com/v1/forecast?latitude="
+                + gardenLatitude
+                + "&longitude=" + gardenLongitude
+                + "&current=temperature_2m,relative_humidity_2m,weather_code&hourly=temperature_2m,relative_humidity_2m,weather_code&past_days=2";
 
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url))
                 .GET()
                 .build();
+
         try {
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            logger.info(response.body());
+            HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+            JSONObject jsonObject = new JSONObject(response.body());
+
+            current = jsonObject.getJSONObject("current");
+            currentTemp = current.getDouble("temperature_2m");
+            currentHumidity = current.getInt("relative_humidity_2m");
+            currentWeather = current.getInt("weather_code");
+
+
+            hourly = jsonObject.getJSONObject("hourly");
+
+            logger.info(String.valueOf(currentHumidity));
+
         } catch (Exception e) {
             logger.error(e.toString());
         }
     }
-
-
 }
