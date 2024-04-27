83803bf77496255d36ca1f23e20eb43a95edf51e
Emma Davis <eda60@uclive.ac.nz>
2024-04-27 17:59:41 +1200

    Added security authentication to UpdateUserPassword (U7 cucumber test), tidied up variables. Identified issue with U4 AC4 - will investigate!!!

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/cucumber/step_definitions/UpdateUserPassword.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/cucumber/step_definitions/UpdateUserPassword.java
@@ -11,29 +11,41 @@ import org.junit.jupiter.api.Assertions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.util.Assert;
+import org.springframework.web.context.WebApplicationContext;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+
 @SpringBootTest
 public class UpdateUserPassword {
 
     @Autowired
     public AuthenticationManager authenticationManager;
-    public static MockMvc MOCK_MVC;
-    @Autowired
-    public UserRepository userRepository;
-    public static UserService userService;
     @Autowired
     public PasswordEncoder passwordEncoder;
+    @Autowired
+    public UserRepository userRepository;
+    @Autowired
     public FileService fileService;
+    @Autowired
+    private WebApplicationContext context;
+
+
+    public static MockMvc MOCK_MVC;
+    public static UserService userService;
+
 
     String currentPassword;
     String newPassword;
     String retypePassword;
     String userEmail;
+    String originalPassword;
     //
     //
     //New Tests:
@@ -44,44 +56,53 @@ public class UpdateUserPassword {
     public void before_or_after_all() {
         userService = new UserService(passwordEncoder, userRepository);
         ProfileController profileController = new ProfileController(userService, authenticationManager, fileService);
-        MOCK_MVC = MockMvcBuilders.standaloneSetup(profileController).build();
+
 
         userEmail = "jamesdoe@email.com";
+        originalPassword = "AlphabetSoup10!";
         // Prep for user authentication that will be added later on
         User user = new User("Admin", "Test", userEmail, null);
         if (!userService.emailInUse(userEmail))
         {
-            userService.addUser(user, "AlphabetSoup10!");
+            userService.addUser(user, originalPassword);
         }
+
+        MOCK_MVC = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(SecurityMockMvcConfigurers.springSecurity())
+                .build();
+
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(userEmail, originalPassword);
+        Authentication authentication = authenticationManager.authenticate(token);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
     }
 
     @When("I enter two different new passwords: {string} and {string}")
     public void iEnterTwoDifferentPasswordsInNew(String newPass,String retypePass) {
-        currentPassword = "AlphabetSoup10!";
+        currentPassword = originalPassword;
         newPassword = newPass;
         retypePassword = retypePass;
     }
     @Then("The password does not get updated")
     public void thePasswordDoesNotGetUpdated() {
         Assertions.assertNull(userService.getUserByEmailAndPassword("jamesdoe@email.com",newPassword));
-        Assertions.assertNotNull(userService.getUserByEmailAndPassword("jamesdoe@email.com",currentPassword));
+        Assertions.assertNotNull(userService.getUserByEmailAndPassword("jamesdoe@email.com",originalPassword));
     }
     @Then("The password is updated")
     public void thePasswordIsUpdated() {
-        System.out.println(userService.getUserByEmailAndPassword("jamesdoe@email.com",currentPassword));
         Assertions.assertNotNull(userService.getUserByEmailAndPassword("jamesdoe@email.com",newPassword));
     }
 
     @When("I enter the weak password: {string}")
     public void iEnterTheWeakPasswordWeakPassword(String weakPassword) {
-        currentPassword = "AlphabetSoup10!";
+        currentPassword = originalPassword;
         newPassword = weakPassword;
         retypePassword = weakPassword;
     }
 
     @When("I enter fully compliant password: {string}")
     public void iEnterFullyCompliantDetails(String compliantPassword) {
-        currentPassword = "AlphabetSoup10!";
+        currentPassword = originalPassword;
         newPassword = compliantPassword;
         retypePassword = compliantPassword;
     }
@@ -90,10 +111,11 @@ public class UpdateUserPassword {
     public void iClickTheSubmitButton() throws Exception {
         MOCK_MVC.perform(
                 MockMvcRequestBuilders
-                        .post("/editPassword")
+                        .post("/profile/editPassword")
                         .param("currentPassword", currentPassword)
                         .param("newPassword", newPassword)
-                        .param("repeatPassword", retypePassword)
+                        .param("retypePassword", retypePassword)
+                        .with(csrf())
         );
     }
 
