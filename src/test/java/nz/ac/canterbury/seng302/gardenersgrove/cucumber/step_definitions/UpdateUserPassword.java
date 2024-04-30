fbfe8c10a082b68d55423021840857b63ec81dab
Luke Edwards <led46@uclive.ac.nz>
2024-04-30 16:27:57 +1200

    Fix: rewrote U7 AC tests to have a background so the tests can be run independenlty of other tests and won't fail

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/cucumber/step_definitions/UpdateUserPassword.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/cucumber/step_definitions/UpdateUserPassword.java
@@ -1,5 +1,6 @@
 package nz.ac.canterbury.seng302.gardenersgrove.cucumber.step_definitions;
 
+import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -19,7 +20,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -36,97 +39,90 @@ public class UpdateUserPassword {
     public UserRepository userRepository;
     @Autowired
     public FileService fileService;
-    @Autowired
-    private WebApplicationContext context;
-
 
     public static MockMvc MOCK_MVC;
     public static UserService userService;
+    private MvcResult editPasswordResult;
 
+    User loggedInUser;
 
     String currentPassword;
-    String newPassword;
-    String retypePassword;
+    String origHash;
     String userEmail;
-    String originalPassword;
-    //
-    //
-    //New Tests:
-    //
-    //
 
-    @Given ("Given i am on the edit password page")
+    @Before
     public void before_or_after_all() {
         userService = new UserService(passwordEncoder, userRepository);
         ProfileController profileController = new ProfileController(userService, authenticationManager, fileService);
+        MOCK_MVC = MockMvcBuilders.standaloneSetup(profileController).build();
 
-
-        userEmail = "jamesdoe@email.com";
-        originalPassword = "AlphabetSoup10!";
-        // Prep for user authentication that will be added later on
-        User user = new User("Admin", "Test", userEmail, null);
-        if (!userService.emailInUse(userEmail))
-        {
-            userService.addUser(user, originalPassword);
-        }
-
-        MOCK_MVC = MockMvcBuilders
-                .webAppContextSetup(context)
-                .apply(SecurityMockMvcConfigurers.springSecurity())
-                .build();
-
-        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(userEmail, originalPassword);
-        Authentication authentication = authenticationManager.authenticate(token);
-        SecurityContextHolder.getContext().setAuthentication(authentication);
     }
+    @Given("I as user {string} with password {string} am on the edit password page")
+    public void iAsUserWithPasswordAmOnTheEditPasswordPage(String userEmail, String currentPassword) throws Exception {
+        String url = "/profile/editPassword";
+        MOCK_MVC.perform(
+                MockMvcRequestBuilders
+                        .get(url)
+
+        ).andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+        loggedInUser = userService.getUserByEmail(userEmail);
+        this.currentPassword = currentPassword;
+        origHash = loggedInUser.getEncodedPassword();
+        this.userEmail = loggedInUser.getEmailAddress();
+    }
+
+
 
     @When("I enter two different new passwords: {string} and {string}")
-    public void i_enter_two_different_passwords_in_new(String newPass,String retypePass) {
-        currentPassword = originalPassword;
-        newPassword = newPass;
-        retypePassword = retypePass;
-    }
-    @Then("The password does not get updated")
-    public void the_password_does_not_get_updated() {
-        Assertions.assertNull(userService.getUserByEmailAndPassword("jamesdoe@email.com",newPassword));
-        Assertions.assertNotNull(userService.getUserByEmailAndPassword("jamesdoe@email.com",originalPassword));
-    }
-    @Then("The password is updated")
-    public void the_password_is_updated() {
-        Assertions.assertNotNull(userService.getUserByEmailAndPassword("jamesdoe@email.com",newPassword));
-    }
-
-    @When("I enter the weak password: {string}")
-    public void i_enter_the_weak_password_weak_password(String weakPassword) {
-        currentPassword = originalPassword;
-        newPassword = weakPassword;
-        retypePassword = weakPassword;
-    }
-
-    @When("I enter fully compliant password: {string}")
-    public void i_enter_fully_compliant_details(String compliantPassword) {
-        currentPassword = originalPassword;
-        newPassword = compliantPassword;
-        retypePassword = compliantPassword;
-    }
-
-    @And("I click the “Submit” button to edit password")
-    public void i_click_the_submit_button() throws Exception {
-        MOCK_MVC.perform(
+    public void i_enter_two_different_passwords_in_new(String newPass,String retypePass) throws Exception {
+        editPasswordResult = MOCK_MVC.perform(
                 MockMvcRequestBuilders
                         .post("/profile/editPassword")
                         .param("currentPassword", currentPassword)
-                        .param("newPassword", newPassword)
-                        .param("retypePassword", retypePassword)
-                        .with(csrf())
-        );
+                        .param("newPassword", newPass)
+                        .param("retypePassword", retypePass)
+        ).andReturn();
+    }
+    @Then("The password does not get updated")
+    public void the_password_does_not_get_updated() {
+        Assertions.assertEquals(origHash, userService.getUserByEmail(userEmail).getEncodedPassword());
+    }
+    @Then("The password is updated")
+    public void the_password_is_updated() {
+        Assertions.assertNotEquals(origHash, userService.getUserByEmail(userEmail).getEncodedPassword());
+    }
+
+    @When("I enter the weak password: {string}")
+    public void i_enter_the_weak_password_weak_password(String weakPassword) throws Exception {
+        editPasswordResult = MOCK_MVC.perform(
+                MockMvcRequestBuilders
+                        .post("/profile/editPassword")
+                        .param("currentPassword", currentPassword)
+                        .param("newPassword", weakPassword)
+                        .param("retypePassword", weakPassword)
+        ).andReturn();
+    }
+
+    @When("I enter fully compliant password: {string}")
+    public void i_enter_fully_compliant_details(String compliantPassword) throws Exception {
+        editPasswordResult = MOCK_MVC.perform(
+                MockMvcRequestBuilders
+                        .post("/profile/editPassword")
+                        .param("currentPassword", currentPassword)
+                        .param("newPassword", compliantPassword)
+                        .param("retypePassword", compliantPassword)
+        ).andReturn();
     }
 
     @When("I enter an old password {string} that does not match the current password")
-    public void i_enter_an_old_password_that_does_not_match_the_current_password(String oldPassword) {
-        currentPassword = oldPassword;
-        newPassword = "TestPass10!";
-        retypePassword = "TestPass10!";
+    public void i_enter_an_old_password_that_does_not_match_the_current_password(String oldPassword) throws Exception {
+        editPasswordResult = MOCK_MVC.perform(
+                MockMvcRequestBuilders
+                        .post("/profile/editPassword")
+                        .param("currentPassword", currentPassword)
+                        .param("newPassword", oldPassword)
+                        .param("retypePassword", oldPassword)
+        ).andReturn();
     }
 
 }
