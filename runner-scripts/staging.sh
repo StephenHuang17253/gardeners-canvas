f47bd26abc1a2e996c4a165a53a9c2d93d058548
Tyler Fauchelle <tfa46@uclive.ac.nz>
2024-05-09 11:56:19 +1200

    Added staging config file, Removed production running its own config for now.

--- runner-scripts/staging.sh
+++ runner-scripts/staging.sh
@@ -5,3 +5,4 @@ java -jar staging/libs/gardeners-grove-0.0.1-SNAPSHOT.jar \
     --server.port=9500 \
     --server.servlet.contextPath=/test \
     --spring.application.name=gardeners-grove
+    --spring.config.location=/resources/application-staging.properties
