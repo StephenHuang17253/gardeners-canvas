bdabe12a12c3485e952153873bb1eab5bbea106b
Tyler Fauchelle <tfa46@uclive.ac.nz>
2024-05-09 20:24:44 +1200

    Tweaked config directory to test if it loads correctly

--- runner-scripts/production.sh
+++ runner-scripts/production.sh
@@ -5,4 +5,4 @@ java -jar production/libs/gardeners-grove-0.0.1-SNAPSHOT.jar \
     --server.port=10500 \
     --server.servlet.contextPath=/prod \
     --spring.application.name=gardeners-grove \
-    --spring.config.location=/resources/application-production.properties
+    --spring.config.location=resources/application-production.properties
