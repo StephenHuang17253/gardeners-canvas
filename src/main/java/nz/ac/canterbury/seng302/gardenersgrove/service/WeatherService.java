106a2479da01ac1ba25d1dad124ce7501f017842
Emma Davis <eda60@uclive.ac.nz>
2024-05-10 12:25:44 +1200

    Added methods to get the past 2 days and the daily forecast for a week (including current day). Returning as JSON Arrays

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/service/WeatherService.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/service/WeatherService.java
@@ -9,24 +9,26 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
 
 public class WeatherService {
 
     Logger logger = LoggerFactory.getLogger(WeatherService.class);
 
     JSONObject current;
-    double currentTemp;
-    int currentHumidity;
-    int currentWeather;
+    JSONObject daily;
 
-    JSONObject hourly;
-
+    JSONArray tempMax;
+    JSONArray tempMin;
+    JSONArray dailyWeatherCodes;
+    JSONArray dailyRain;
+    JSONArray dates;
 
     public void getWeather(String gardenLatitude, String gardenLongitude) {
         String url = "https://api.open-meteo.com/v1/forecast?latitude="
                 + gardenLatitude
                 + "&longitude=" + gardenLongitude
-                + "&current=temperature_2m,relative_humidity_2m,weather_code&hourly=temperature_2m,relative_humidity_2m,weather_code&past_days=2";
+                + "&current=temperature_2m,relative_humidity_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min,rain_sum&timezone=auto&past_days=2";
 
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url))
@@ -38,17 +40,98 @@ public class WeatherService {
             JSONObject jsonObject = new JSONObject(response.body());
 
             current = jsonObject.getJSONObject("current");
-            currentTemp = current.getDouble("temperature_2m");
-            currentHumidity = current.getInt("relative_humidity_2m");
-            currentWeather = current.getInt("weather_code");
+            daily = jsonObject.getJSONObject("daily");
 
-
-            hourly = jsonObject.getJSONObject("hourly");
-
-            logger.info(String.valueOf(currentHumidity));
+            getDailyWeather();
+            getForecast();
 
         } catch (Exception e) {
             logger.error(e.toString());
         }
     }
+
+    public JSONObject getCurrentWeather() {
+        return current;
+    }
+
+    public double getCurrentTemperature() {
+        return current.getDouble("temperature_2m");
+    }
+
+    public double getCurrentHumidity() {
+        return current.getDouble("relative_humidity_2m");
+    }
+
+    public int getCurrentWeatherCode() {
+        return current.getInt("weather_code");
+    }
+
+    public JSONObject getDailyWeather() {
+        tempMax =  daily.getJSONArray("temperature_2m_max");
+        tempMin = daily.getJSONArray("temperature_2m_min");
+        dailyWeatherCodes = daily.getJSONArray("weather_code");
+        dailyRain = daily.getJSONArray("rain_sum");
+        dates = daily.getJSONArray("time");
+
+        return daily;
+    }
+
+    public JSONObject getPastWeather() {
+
+        JSONObject pastWeather = new JSONObject();
+        JSONArray pastTempMax = new JSONArray();
+        JSONArray pastTempMin = new JSONArray();
+        JSONArray pastWeatherCodes = new JSONArray();
+        JSONArray pastRain = new JSONArray();
+        JSONArray pastDates = new JSONArray();
+
+       for (int i = 0; i < 2; i++) {
+           pastTempMax.put(tempMax.get(i));
+           pastTempMin.put(tempMin.get(i));
+           pastWeatherCodes.put(dailyWeatherCodes.get(i));
+           pastRain.put(dailyRain.get(i));
+           pastDates.put(dates.get(i));
+
+       }
+
+       pastWeather.put("temperature_max", pastTempMax);
+       pastWeather.put("temperature_min", pastTempMin);
+       pastWeather.put("weather_code", pastWeatherCodes);
+       pastWeather.put("rain", pastRain);
+       pastWeather.put("date", pastDates);
+
+       logger.info(pastWeather.toString());
+
+       return pastWeather;
+    }
+
+    public JSONObject getForecast() {
+
+        JSONObject forecastWeather = new JSONObject();
+        JSONArray forecastTempMax = new JSONArray();
+        JSONArray forecastTempMin = new JSONArray();
+        JSONArray forecastWeatherCodes = new JSONArray();
+        JSONArray forecastRain = new JSONArray();
+        JSONArray forecastDates = new JSONArray();
+
+        for (int i = 2; i < tempMax.length(); i++) {
+            forecastTempMax.put(tempMax.get(i));
+            forecastTempMin.put(tempMin.get(i));
+            forecastWeatherCodes.put(dailyWeatherCodes.get(i));
+            forecastRain.put(dailyRain.get(i));
+            forecastDates.put(dates.get(i));
+
+        }
+
+        forecastWeather.put("temperature_max", forecastTempMax);
+        forecastWeather.put("temperature_min", forecastTempMin);
+        forecastWeather.put("weather_code", forecastWeatherCodes);
+        forecastWeather.put("rain", forecastRain);
+        forecastWeather.put("date", forecastDates);
+
+        logger.info(forecastWeather.toString());
+
+        return forecastWeather;
+    }
+
 }
