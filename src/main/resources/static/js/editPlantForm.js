a8873ab64aa9fcf8e3f8d57bf9cc15010204b7d5
Aditi Sharma <ash313@uclive.ac.nz>
2024-05-19 12:25:16 +1200

    Changed plantDescription textarea to only allow 512 characters - this has been approved by the PO - has been done on both editPlantForm.html and createNewPlantForm.html - also showing error when users attempt to enter in more than 512 characters

--- src/main/resources/static/js/editPlantForm.js
+++ src/main/resources/static/js/editPlantForm.js
@@ -1,7 +1,12 @@
-// Event listener ensuring more than 1000 characters not entered.
+let DESCRIPTION_LIMIT = 512
+// Event listener ensuring more than 512 characters not entered.
 document.getElementById('plantDescription').addEventListener('input', function(event) {
+    let limit = DESCRIPTION_LIMIT;
     let input = event.target.value;
-    if (input.length > 1000) {
-        document.getElementById('plantDescription').value = input.slice(0, 1000);
+    if (input.length > limit) {
+        document.getElementById('plantDescription').value = input.slice(0, limit);
+        document.getElementById('plantDescriptionError').textContent = "Plant description must be less than " + limit + " characters";
+    } else {
+        document.getElementById('plantDescriptionError').textContent = "";
     }
 });
