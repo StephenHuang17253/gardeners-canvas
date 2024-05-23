9a61e1541c061ea44af3bcfcf194982f54f71256
Aditi Sharma <ash313@uclive.ac.nz>
2024-05-24 10:30:25 +1200

    found a better way to hide search bar until it is needed - this keeps html in htmls, and js is only for click on the add friend button

--- src/main/resources/static/js/manageFriendsPage.js
+++ src/main/resources/static/js/manageFriendsPage.js
@@ -2,21 +2,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const addFriendButton = document.getElementById('addFriendButton');
     const searchBarContainer = document.getElementById('searchBarContainer');
-
     addFriendButton.addEventListener('click', function() {
-        // Check if the search bar is already present
-        if (!document.getElementById('friendSearchBar')) {
-            const searchBar = document.createElement('input');
-            searchBar.setAttribute('type', 'text');
-            searchBar.setAttribute('id', 'friendSearchBar');
-            searchBar.setAttribute('placeholder', 'Search');
-            searchBar.classList.add('search-bar');
-            searchBarContainer.appendChild(searchBar);
-            const search = document.createElement('button');
-            search.setAttribute('id', 'friendSearchSubmitButton');
-            search.textContent = 'Search';
-            search.classList.add('user_button');
-            searchBarContainer.appendChild(search);
-        }
+        searchBarContainer.style.display = 'block';
     });
 });
