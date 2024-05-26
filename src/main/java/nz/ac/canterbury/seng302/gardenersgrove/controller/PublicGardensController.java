92e63c03b585635219d1baf50dba136a3c54fc33
Tyler <tfa46@uclive.ac.nz>
2024-05-26 16:55:51 +1200

    Added basic pagination on public gardens page by changing public-gardens/page/1 <- pagination number. Can only navigate using URL currently. Increased amount of John's gardens examples to be able to test the pagination

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/PublicGardensController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/PublicGardensController.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Comparator;
 import java.util.List;
@@ -38,6 +39,27 @@ public class PublicGardensController {
      *
      * @return thymeleaf BrowsePublicGardens html element
      */
+    @GetMapping("/public-gardens/page/{selection}")
+    public String publicGardensPagination(@PathVariable Long selection, Model model) {
+        logger.info("GET /public-gardens");
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        boolean loggedIn = authentication != null && authentication.getName() != "anonymousUser";
+        model.addAttribute("loggedIn", loggedIn);
+
+        List<Garden> allGardens = gardenService.getGardens();
+
+        List<Garden> tenSortedPublicGardens = allGardens.stream()
+                .sorted(Comparator.comparing(Garden::getCreationDate))
+                .skip(selection * 10L)
+                .limit(10L) // Temporary limit until pagination is implemented.
+                .collect(Collectors.toList());
+
+        model.addAttribute("publicGardens", tenSortedPublicGardens);
+
+        return "BrowsePublicGardens";
+    }
+
     @GetMapping("/public-gardens")
     public String publicGardens(Model model) {
         logger.info("GET /public-gardens");
