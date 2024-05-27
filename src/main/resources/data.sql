c5c68dc30825599e1cee4a93f2b7852bb6c85c86
Luke Edwards <led46@uclive.ac.nz>
2024-05-27 12:11:39 +1200

    Feature: Add data.sql with pre-existing users and saved static images

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO USER_TABLE (DATE_OF_BIRTH,VERIFIED,USER_ID,FIRST_NAME,LAST_NAME,PROFILE_PICTURE_FILENAME,EMAIL_ADDRESS,PASSWORD)
+VALUES
+('2001-01-01', TRUE, 1, 'Sheldon','Cooper',NULL,'sheldoncopper@email.com','{bcrypt}$2a$10$NMK7UwogRwxjjMlOqHjF9.8XSdleK4qzjd6tCEksLP3Bp34SnWBoq'),
+('1975-05-01', TRUE, 2, 'George', 'Cooper', '../../images/user_2_profile_picture.jpg', 'georgecoopersr@email.com', '{bcrypt}$2a$10$NMK7UwogRwxjjMlOqHjF9.8XSdleK4qzjd6tCEksLP3Bp34SnWBoq'),
+('1992-11-25', TRUE, 3, 'George', 'Cooper', '../../images/user_3_profile_picture.jpg', 'georgecooperjr@email.com', '{bcrypt}$2a$10$NMK7UwogRwxjjMlOqHjF9.8XSdleK4qzjd6tCEksLP3Bp34SnWBoq');
