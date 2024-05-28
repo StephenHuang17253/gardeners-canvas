939ab189e5eeaf3c2503bfcd6006d950cad7735a
Aditi Sharma <ash313@uclive.ac.nz>
2024-05-28 12:49:31 +1200

    Implemented cucumber tests and they are all passing

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/cucumber/step_definitions/BrowsePublicGardens.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/cucumber/step_definitions/BrowsePublicGardens.java
@@ -1,45 +1,152 @@
 package nz.ac.canterbury.seng302.gardenersgrove.cucumber.step_definitions;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import nz.ac.canterbury.seng302.gardenersgrove.controller.PublicGardensController;
+import nz.ac.canterbury.seng302.gardenersgrove.entity.Garden;
+import nz.ac.canterbury.seng302.gardenersgrove.entity.User;
+import nz.ac.canterbury.seng302.gardenersgrove.repository.GardenRepository;
+import nz.ac.canterbury.seng302.gardenersgrove.repository.PlantRepository;
+import nz.ac.canterbury.seng302.gardenersgrove.repository.UserRepository;
+import nz.ac.canterbury.seng302.gardenersgrove.service.*;
+import org.junit.jupiter.api.Assertions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
 public class BrowsePublicGardens {
 
-    @Given("{string} has {int} plants.")
-    public void has_plants(String gardenName, Integer plantNo) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
+    public static MockMvc MOCK_MVC;
 
-    @Given("I am on the browse gardens page")
-    public void i_am_on_the_browse_gardens_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    @Autowired
+    public GardenRepository gardenRepository;
+
+    @Autowired
+    public PlantRepository plantRepository;
+
+    @Autowired
+    public UserRepository userRepository;
+
+    @Autowired
+    public PasswordEncoder passwordEncoder;
+
+
+    @Autowired
+    public SecurityService securityService;
+
+
+    public static GardenService gardenService;
+
+    public static UserService userService;
+
+    public static PlantService plantService;
+
+    public static FileService fileService;
+
+    private String searchValue;
+
+    private MvcResult mvcResult;
+
+
+    @Given("I am browsing gardens")
+    public void before_or_after_all() throws IOException, InterruptedException {
+
+        fileService = new FileService();
+        userService = new UserService(passwordEncoder, userRepository);
+        gardenService = new GardenService(gardenRepository, userService);
+        plantService = new PlantService(plantRepository, gardenService, fileService);
+
+        PublicGardensController publicGardensController = new PublicGardensController(gardenService);
+
+        // Allows us to bypass spring security
+        MOCK_MVC = MockMvcBuilders
+                .standaloneSetup(publicGardensController)
+                .build();
     }
 
     @Given("I enter the search string {string}")
     public void i_enter_the_search_string(String input) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        searchValue = input;
     }
 
-    @Given("I hit the search button")
-    public void i_hit_the_search_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    @Given("I added {string} {string}, {int} is a user with email {string} and password {string}")
+    public void i_added_is_a_user_with_email_and_password(String firstName, String LastName, Integer age, String userEmail, String userPassword) {
+        if (userService.getUserByEmail(userEmail) == null) {
+            int birthYear = 2024 - age;
+            String dob = "01/01/" + birthYear;
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy", Locale.ENGLISH);
+            LocalDate dateOfBirth = LocalDate.parse(dob, formatter);
+
+            User user = new User(firstName, LastName, userEmail, dateOfBirth);
+            user.setVerified(true);
+            userService.addUser(user, userPassword);
+            Assertions.assertNotNull(userService.getUserByEmail(userEmail));
+        }
+
+    }
+
+    @When("I hit the search button with page {int}")
+    public void i_hit_the_search_button_with_page(Integer page) throws Exception {
+        mvcResult = MOCK_MVC.perform(
+                        MockMvcRequestBuilders
+                                .get("/public-gardens/search/" + page)
+                                .param("searchInput", searchValue))
+                .andExpect(status().isOk())
+                .andReturn();
     }
 
     @Then("I am shown only gardens whose names or plants include my search string {string}")
     public void i_am_shown_only_gardens_whose_names_or_plants_include_my_search_string(String input) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        List<Garden> searchResults = (List<Garden>) mvcResult.getModelAndView().getModelMap().getAttribute("publicGardens");
+        assert searchResults != null;
+        for (Garden result : searchResults) {
+            Assertions.assertTrue(result.getGardenName().contains(input) || result.getPlants().stream().anyMatch(plant -> plant.getPlantName().contains(input)));
+        }
+
     }
 
-    @Then("A message tells me “No gardens match your search”.")
-    public void a_message_tells_me_no_gardens_match_your_search() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    @Then("A message tells me {string}")
+    public void a_message_tells_me(String error) {
+        String searchError = (String) mvcResult.getModelAndView().getModelMap().getAttribute("SearchErrorText");
+        Assertions.assertEquals(error, searchError);
     }
 
+    @And("User {string} has a garden {string} located in {string}, {string} with {int} plants.")
+    public void user_has_a_garden_located_in_with_plants(String email, String gardenName, String city, String country, int plantNo) {
+        User user = userService.getUserByEmail(email);
+        if (user.getGardens().stream().noneMatch(garden -> Objects.equals(garden.getGardenName(), gardenName))) {
+            Garden garden = new Garden(gardenName, "", "", city, "", country, 3.0, user);
+            garden = gardenService.addGarden(garden);
+            for (int i = 0; i < plantNo; i++) {
+                String plantName = gardenName + " " + (i + 1);
+                plantService.addPlant(plantName, 2, gardenName, LocalDate.now(), garden.getGardenId());
+            }
+        }
 
+    }
+
+    @Transactional
+    @And("I clear the persistence")
+    public void i_clear_the_persistence() {
+
+
+    }
 }
--- src/test/resources/features/u20-browsing-public-gardens.feature
+++ src/test/resources/features/u20-browsing-public-gardens.feature
@@ -2,43 +2,40 @@ Feature: U20 (Browsing public gardens): As Inaya, I want to be able to browse ga
   have made public so that I can learn from what other gardeners are doing.
 
   Background:
-    Given "Inaya" "Singh", 24 is a user with email "inaya123@email.com" and password "TestPassword10!"
-    And User "inaya123@email.com" has a garden "Inaya's Garden" located in "Dunedin", "New Zealand"
-    And "Inaya's Garden" has 13 plants.
-    And "Jerry" "Thomas", 60 is a user with email "jerry@email.com" and password "Cheese123!"
-    And User "jerry@email.com" has a garden "Jerry's Garden" located in "Christchurch", "New Zealand"
-    And "Jerry's Garden" has 10 plants.
-    And "Thomas" "Jerry", 50 is a user with email "thomas@email.com" and password "TestPassword123!"
-    And User "tom@email.com" has a garden "Tom's Garden" located in "Auckland", "New Zealand"
-    And "Tom's Garden" has 2 plants.
+    Given I am browsing gardens
+    And I added "Inaya" "Singh", 24 is a user with email "inaya123@email.com" and password "TestPassword10!"
+    And I added "Jerry" "Thomas", 60 is a user with email "jerry@email.com" and password "Cheese123!"
+    And I added "Thomas" "Jerry", 50 is a user with email "thomas@email.com" and password "TestPassword123!"
+    And User "inaya123@email.com" has a garden "Inaya's Garden" located in "Dunedin", "New Zealand" with 13 plants.
+    And User "jerry@email.com" has a garden "Jerry's Garden" located in "Christchurch", "New Zealand" with 10 plants.
+    And User "thomas@email.com" has a garden "Tom's Garden" located in "Auckland", "New Zealand" with 2 plants.
     And I as user "inaya123@email.com" am logged in with "TestPassword10!"
-    And I am on the browse gardens page
 
+  @NotRequiresSetup
   Scenario Outline: AC3 - Given I enter a search string and click a search button either labelled “search” or with a
   magnifying glass icon, then I am shown only gardens whose names or plants include my search value.
 
     Given I enter the search string <input>
-    And I hit the search button
+    When I hit the search button with page <page>
     Then I am shown only gardens whose names or plants include my search string <input>
+    And I clear the persistence
     Examples:
-      | input     |
-      | "Amy Doe" |
-      | "Andy"    |
+      | input    | page |
+      | "1"      | 1    |
+      | "Jerry"  | 1    |
+      | "Garden" | 1    |
 
+  @NotRequiresSetup
   Scenario Outline: AC5 - Given I enter a search string that has no matches, then a message tells me
   “No gardens match your search”.
 
     Given I enter the search string <input>
-    And I hit the search button
-    Then A message tells me “No gardens match your search”.
+    When I hit the search button with page <page>
+    Then A message tells me "No gardens match your search"
+    And I clear the persistence
     Examples:
-      | input              |
-      | "Andy"             |
-      | "random@gmail.com" |
-      | "liam@email.com"   |
-      | "Andy Doe"         |
-      | "sjdlkfjaljlf;"    |
-      | ""                 |
+      | input   | page |
+      | "Emily" | 1    |
 
 
 
