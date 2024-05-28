377172993dbc4ad3ee872a04fffb94a6da2e1057
Lachlan Stewart <lst117@uclive.ac.nz>
2024-05-28 13:37:35 +1200

    changed constant variable name to upper case

--- src/main/resources/static/js/inputCharacterLimit.js
+++ src/main/resources/static/js/inputCharacterLimit.js
@@ -1,12 +1,12 @@
 var textAreas = document.getElementsByTagName('textarea');
 var inputs = document.getElementsByTagName('input');
 
-const maxLength = 5000;
+const MAXLENGTH = 5000;
 
 for (var i = 0; i < textAreas.length; i++) {
-    textAreas[i].maxLength = maxLength;
+    textAreas[i].maxLength = MAXLENGTH;
 }
 
 for (var i = 0; i < inputs.length; i++) {
-    inputs[i].maxLength = maxLength;
+    inputs[i].maxLength = MAXLENGTH;
 }
