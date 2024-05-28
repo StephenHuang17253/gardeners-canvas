e748e1f8255dac1fcbba16c6eaa8950541b10c06
Lachlan Stewart <lst117@uclive.ac.nz>
2024-05-28 13:59:31 +1200

    changed name to follow naming convention in inputCharacterLimit.js

--- src/main/resources/static/js/inputCharacterLimit.js
+++ src/main/resources/static/js/inputCharacterLimit.js
@@ -1,12 +1,12 @@
 var textAreas = document.getElementsByTagName('textarea');
 var inputs = document.getElementsByTagName('input');
 
-const MAXLENGTH = 5000;
+const MAX_LENGTH = 5000;
 
 for (var i = 0; i < textAreas.length; i++) {
-    textAreas[i].maxLength = MAXLENGTH;
+    textAreas[i].maxLength = MAX_LENGTH;
 }
 
 for (var i = 0; i < inputs.length; i++) {
-    inputs[i].maxLength = MAXLENGTH;
+    inputs[i].maxLength = MAX_LENGTH;
 }
