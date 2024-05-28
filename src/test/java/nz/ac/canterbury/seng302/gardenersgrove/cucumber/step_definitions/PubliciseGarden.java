be7070c1e4f5f1dc43ed6fff9329621a8c3fc2a8
eda60 <eda60@uclive.ac.nz>
2024-05-28 18:11:45 +1200

    Began writing the cucumber tests for the rest of U19 ACs (2-6). Wrote the feature structure and began implementing the tests. All are running except for AC4, where the garden is not being updated as expected.

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/cucumber/step_definitions/PubliciseGarden.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/cucumber/step_definitions/PubliciseGarden.java
@@ -1,25 +1,32 @@
 package nz.ac.canterbury.seng302.gardenersgrove.cucumber.step_definitions;
 
 import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import nz.ac.canterbury.seng302.gardenersgrove.controller.GardenFormController;
 import nz.ac.canterbury.seng302.gardenersgrove.controller.MyGardensController;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.Garden;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.User;
 import nz.ac.canterbury.seng302.gardenersgrove.repository.GardenRepository;
 import nz.ac.canterbury.seng302.gardenersgrove.repository.UserRepository;
 import nz.ac.canterbury.seng302.gardenersgrove.service.*;
+import nz.ac.canterbury.seng302.gardenersgrove.validation.inputValidation.InputValidator;
 import org.junit.jupiter.api.Assertions;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Optional;
 
@@ -28,7 +35,9 @@ import static org.springframework.security.test.web.servlet.request.SecurityMock
 @SpringBootTest
 public class PubliciseGarden {
 
-    public static MockMvc MOCK_MVC;
+    public static MockMvc MOCK_MVC_MY_GARDEN;
+
+    public static MockMvc MOCK_MVC_GARDEN_FORM;
 
     @Autowired
     public GardenRepository gardenRepository;
@@ -45,6 +54,9 @@ public class PubliciseGarden {
     @Autowired
     public SecurityService securityService;
 
+    @MockBean
+    private LocationService locationService;
+
     private static GardenService gardenService;
 
     private static UserService userService;
@@ -52,35 +64,50 @@ public class PubliciseGarden {
     private static PlantService plantService;
 
     private static FileService fileService;
+    private ProfanityService profanityService;
+
+    private InputValidator inputValidator;
 
     private User user;
 
     private Garden userGarden;
 
+    private Garden newGarden;
+
+    private String description;
+
+    private MvcResult createGardenResult;
+
 
     @Before
     public void before_or_after_all() {
+
+        profanityService = Mockito.mock(ProfanityService.class);
+        inputValidator = new InputValidator(userService, profanityService);
+
+        Mockito.when(profanityService.containsProfanity(Mockito.anyString())).thenReturn(false);
+
         userService = new UserService(passwordEncoder, userRepository);
         gardenService = new GardenService(gardenRepository, userService);
         MyGardensController myGardensController = new MyGardensController(gardenService, securityService, plantService, fileService);
-        MOCK_MVC = MockMvcBuilders.standaloneSetup(myGardensController).build();
+        GardenFormController gardenFormController = new GardenFormController(gardenService,locationService,securityService);
+
+        MOCK_MVC_MY_GARDEN = MockMvcBuilders.standaloneSetup(myGardensController).build();
+        MOCK_MVC_GARDEN_FORM = MockMvcBuilders.standaloneSetup(gardenFormController).build();
 
     }
-
+//    AC1
     @Given("User {string} is on my garden details page for {string}")
     public void userIsOnMyGardenDetailsPageFor(String userEmail, String garden) {
        user = userService.getUserByEmail(userEmail);
        userGarden = user.getGardens().get(0);
        Assertions.assertEquals(garden, userGarden.getGardenName());
     }
-
+//    AC1
     @When("I mark a checkbox labelled \"Make my garden public\"")
     public void iMarkACheckboxLabelledMakeMyGardenPublic() throws Exception {
-        System.out.println(SecurityContextHolder.getContext());
-        System.out.println(user.getId());
-
        String myGardenUrl = String.format("/my-gardens/%d/public", userGarden.getGardenId());
-        MOCK_MVC.perform(
+        MOCK_MVC_MY_GARDEN.perform(
                         MockMvcRequestBuilders
                                 .post(myGardenUrl)
                                 .param("makeGardenPublic", String.valueOf(true))
@@ -89,9 +116,101 @@ public class PubliciseGarden {
 
     }
 
+    //    AC1
     @Then("My garden will be visible in search results")
     public void myGardenWillBeVisibleInSearchResults() {
         Optional<Garden> testGarden = gardenService.getGardenById(userGarden.getGardenId());
         Assertions.assertTrue(testGarden.get().getIsPublic());
     }
+//    AC2, AC4
+    @When("I add an optional description")
+    public void iAddAnOptionalDescription() {
+        description = "is a vegetable patch";
+    }
+
+//    AC3,AC5
+    @When("I remove the description of the garden")
+    public void iRemoveTheDescriptionOfTheGarden() {
+        description = "";
+    }
+
+    @And("I am creating a new garden {string}")
+    public void iAmCreatingANewGarden(String gardenName) throws Exception {
+        createGardenResult = MOCK_MVC_GARDEN_FORM.perform(
+                MockMvcRequestBuilders
+                        .post("/create-new-garden")
+                        .param("gardenName", gardenName)
+                        .param("gardenDescription", description)
+                        .param("streetAddress", "")
+                        .param("suburb", "")
+                        .param("city", "Christchurch")
+                        .param("country", "New Zealand")
+                        .param("postcode", "")
+                        .param("gardenSize", "")
+                        .with(csrf())
+        ).andReturn();
+    }
+
+    @And("I am editing an existing garden")
+    public void iAmEditingAnExistingGarden() throws Exception {
+        System.out.println(userGarden.getGardenId());
+        String gardenUrl = String.format("/my-gardens/%d/edit", userGarden.getGardenId());
+        MOCK_MVC_GARDEN_FORM.perform(
+                MockMvcRequestBuilders
+                        .post(gardenUrl)
+                        .param("gardenName", userGarden.getGardenName())
+                        .param("gardenDescription", description)
+                        .param("streetAddress", userGarden.getGardenAddress())
+                        .param("suburb", userGarden.getGardenSuburb())
+                        .param("city", userGarden.getGardenCity())
+                        .param("country", userGarden.getGardenCountry())
+                        .param("postcode", userGarden.getGardenPostcode())
+                        .param("gardenSize", String.valueOf(userGarden.getGardenSize()))
+        ).andExpect(MockMvcResultMatchers.status().isOk());
+
+
+    }
+
+    @Then("the new description is persisted")
+    public void theNewDescriptionIsPersisted() {
+        newGarden = gardenService.getAllUsersGardens(user.getId()).getLast();
+        Assertions.assertEquals(description, newGarden.getGardenDescription());
+
+    }
+
+    @Then("the new description is deleted")
+    public void theNewDescriptionIsDeleted() {
+        newGarden = gardenService.getAllUsersGardens(user.getId()).getLast();
+        Assertions.assertEquals("", newGarden.getGardenDescription());
+    }
+
+    @Then("the edited description is persisted")
+    public void theEditedDescriptionIsPersisted() {
+        Optional<Garden> testGarden = gardenService.getGardenById(userGarden.getGardenId());
+        Assertions.assertEquals(description, testGarden.get().getGardenDescription());
+    }
+
+    @Then("the edited description is deleted")
+    public void theEditedDescriptionIsDeleted() {
+        Optional<Garden> testGarden = gardenService.getGardenById(userGarden.getGardenId());
+        Assertions.assertEquals("", testGarden.get().getGardenDescription());
+    }
+
+    @When("I enter an invalid description {string}")
+    public void iEnterAnInvalidDescription(String invalidDescription) {
+        description = invalidDescription;
+    }
+
+    @Then("an error message appears")
+    public void anErrorMessageAppears(){
+        System.out.println(createGardenResult.getModelAndView());
+        ModelAndView modelAndView = createGardenResult.getModelAndView();
+        Assertions.assertNotNull(modelAndView);
+        String model = modelAndView.getModel().toString();
+
+        Assertions.assertTrue(model.contains("Description must be 512 characters or less and contain some text"));
+    }
+
+
+
 }
--- src/test/resources/features/u19-publicise-gardens.feature
+++ src/test/resources/features/u19-publicise-gardens.feature
@@ -4,8 +4,43 @@ Feature: U19 As Inaya, I want to be able to make my garden public so that others
     Given "Kaia" "Pene", 67 is a user with email "kaia@email.com" and password "TestPassword10!"
     And User "kaia@email.com" has a garden "Kaia's Public Garden" located in "Christchurch", "New Zealand"
     And I as user "kaia@email.com" am logged in with "TestPassword10!"
+    Given User "kaia@email.com" is on my garden details page for "Kaia's Public Garden"
 
   Scenario: AC1 - When I mark my garden as public, my garden is visible in search results
-    Given User "kaia@email.com" is on my garden details page for "Kaia's Public Garden"
     When I mark a checkbox labelled "Make my garden public"
     Then My garden will be visible in search results
+
+Scenario: AC2 - When I add an optional description of the garden, the description is persisted
+  When I add an optional description
+  And I am creating a new garden "Kaia's Described Garden"
+  Then the new description is persisted
+
+
+Scenario: AC3 - When I remove the description of the garden, the description is deleted
+  When I remove the description of the garden
+  And I am creating a new garden "Kaia's Blank Garden"
+  Then the new description is deleted
+
+Scenario: AC4 - When I am editing a garden and add an optional description, the description is persisted
+  When I add an optional description
+  And I am editing an existing garden
+  Then the edited description is persisted
+
+
+Scenario: AC5 - When editing a garden and delete the description of the garden, the description is deleted
+  When I remove the description of the garden
+  And I am editing an existing garden
+  Then the edited description is deleted
+
+Scenario Outline: AC6 - When I enter an invalid description, an error message appears
+  When I enter an invalid description <description>
+  And I am creating a new garden "Kaia's Invalid Garden"
+  Then an error message appears
+  Examples:
+  | description|
+  | '1234%#!#' |
+
+
+
+
+
