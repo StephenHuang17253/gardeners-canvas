192aab2697b53ad2534706580bbd2fc60a2ac50c
Finlay Widdowson <fwi28@uclive.ac.nz>
2024-05-28 23:18:41 +1200

    Moved scrollable container before merging with dev changes that include weather info that would cause a big conflict

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/repository/UserRepository.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package nz.ac.canterbury.seng302.gardenersgrove.repository;
 
 import nz.ac.canterbury.seng302.gardenersgrove.entity.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
