94a08348695a2691229c43a9f9393a23913de8f7
Lachlan Stewart <lst117@uclive.ac.nz>
2024-06-28 15:37:19 +1200

    removed updates to login page, fixed navigation with navbar, refactored navBar.js

--- src/main/resources/static/js/navBar.js
+++ src/main/resources/static/js/navBar.js
@@ -1,7 +1,19 @@
-// Check the pathname to see if app is deployed
-function checkInstance(currentUrl) {
-    // If app is deployed, add correct instance to url
-    let instance = "";
+
+// Some jQuery to make the dropdown open on hover instead of on click
+// Found here: https://www.geeksforgeeks.org/how-to-make-menu-dropdown-on-hover-using-bootstrap/
+$(document).ready(() => {
+    $('.dropdown').hover(() => {
+        $(this).addClass('show');
+        $(this).find('.dropdown-menu').addClass('show');
+    }, () => {
+        $(this).removeClass('show');
+        $(this).find('.dropdown-menu').removeClass('show');
+    });
+});
+
+const getBaseUrl = (currentUrl) => {
+    // Get the correct base url, taking into account if the app is deployed on test or prod
+    let instance = '';
     if (currentUrl.includes('/test/')) {
         instance = '/test';
     } else if (currentUrl.includes('/prod/')) {
@@ -10,103 +22,18 @@ function checkInstance(currentUrl) {
     return instance;
 }
 
-function createGarden() {
-    let currentUrl = window.location.pathname;
-
-
-    //if currentURL is not create new garden send them to this page
-    if (currentUrl !== '/create-new-garden') {
-        localStorage.setItem('previousUrl', window.location.href);
-
-        // Redirect to create-new-garden page
-        let instance = checkInstance(currentUrl)
-        window.location.href = `${instance}/create-new-garden`;
-
-    }
-    const previousUrl = localStorage.getItem('previousUrl');
-    // Default the return URL to home page if not set
-    if (!previousUrl) {
-        localStorage.setItem('previousUrl', '/home');
-    }
-
-}
-
-function openRegisterPage() {
+const navigateTo = (toUrl) => {
     const currentUrl = window.location.pathname;
 
-    // if currentURL is not register send them to this page
-    if (currentUrl !== '/register') {
+    // if currentURL is not toUrl send them to this page
+    // Does not check for test and prod, see git lab issue
+    if (currentUrl !== toUrl) {
         localStorage.setItem('previousUrl', window.location.href);
-
-        // Redirect to create-new-garden page
-        let instance = checkInstance(currentUrl)
-        window.location.href = `${instance}/register`;
-
+        const baseUrl = getBaseUrl(currentUrl);
+        window.location.href = baseUrl + toUrl;
     }
-    const previousUrl = localStorage.getItem('previousUrl');
-    // Default the return URL to home page if not set
-    if (!previousUrl) {
+
+    if (!localStorage.getItem('previousUrl')) {
         localStorage.setItem('previousUrl', '/home');
     }
-
-}
-
-function openLoginPage() {
-    const currentUrl = window.location.pathname;
-
-    // if currentURL is not login send them to this page
-    if (currentUrl !== '/login') {
-        localStorage.setItem('previousUrl', window.location.href);
-
-        // Redirect to create-new-garden page
-        let instance = checkInstance(currentUrl)
-        window.location.href = `${instance}/login`;
-
-    }
-    var previousUrl = localStorage.getItem('previousUrl');
-    // Default the return URL to home page if not set
-    if (!previousUrl) {
-        localStorage.setItem('previousUrl', '/home');
-    }
-}
-
-function openManageFriendsPage() {
-    const currentUrl = window.location.pathname;
-
-    // if currentURL is not manage-friends send them to this page
-    if (currentUrl !== '/manage-friends') {
-        localStorage.setItem('previousUrl', window.location.href);
-
-        // Redirect to manage-friends page
-        let instance = checkInstance(currentUrl)
-        window.location.href = `${instance}/manage-friends#friends`;
-
-    }
-    const previousUrl = localStorage.getItem('previousUrl');
-    // Default the return URL to home page if not set
-    if (!previousUrl) {
-        localStorage.setItem('previousUrl', '/home');
-    }
-
-}
-
-
-function openPublicGardens() {
-    var currentUrl = window.location.pathname;
-
-    // if currentURL is not /public-gardens send them to this page
-    if (currentUrl !== '/public-gardens') {
-        localStorage.setItem('previousUrl', window.location.href);
-
-        // Redirect to create-new-garden page
-        let instance = checkInstance(currentUrl)
-        window.location.href = `${instance}/public-gardens`;
-
-    }
-    var previousUrl = localStorage.getItem('previousUrl');
-    // Default the return URL to home page if not set
-    if (!previousUrl) {
-        localStorage.setItem('previousUrl', '/home');
-    }
-
 }
