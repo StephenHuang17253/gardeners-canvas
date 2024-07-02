a30a4a068e938866884954560d9741a0b3bc0a0d
lst117 <lst117@uclive.ac.nz>
2024-07-03 01:25:47 +1200

    Created errorhandlecontroller to deal with returning thr correct error page depending on the code

---
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/ErrorHandleController.java
@@ -0,0 +1,42 @@
+package nz.ac.canterbury.seng302.gardenersgrove.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+public class ErrorHandleController implements ErrorController {
+
+    Logger logger = LoggerFactory.getLogger(ErrorHandleController.class);
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        
+        logger.error("An Error Ocurred");
+
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status == null) {
+            return "500";
+        }
+
+        Integer statusCode = Integer.valueOf(status.toString());
+        switch (statusCode) {
+            case 404 -> {
+                return "404";
+            }
+            case 403 -> {
+                return "403";
+            }
+            default -> {
+                return "500";
+            }
+        }
+
+    }
+}
