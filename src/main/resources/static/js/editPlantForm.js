5ee2475b93fe9fc82d6217558c9fc723986592fe
Dominic Gorny <dgo65@uclive.ac.nz>
2024-07-04 23:23:51 +1200

    Fix: Updated edit plant form to also check description size limits on page re load to ensure error message persists if form is submitted

--- src/main/resources/static/js/editPlantForm.js
+++ src/main/resources/static/js/editPlantForm.js
@@ -1,12 +1,18 @@
 let DESCRIPTION_LIMIT = 512
 // Event listener ensuring more than 512 characters not entered.
-document.getElementById('plantDescription').addEventListener('input', function(event) {
+
+const description_limit_function = function() {
     let limit = DESCRIPTION_LIMIT;
-    let input = event.target.value;
-    if (input.length > limit) {
+    let input = document.getElementById('plantDescription').value
+
+    if (input.length >= limit) {
         document.getElementById('plantDescription').value = input.slice(0, limit);
         document.getElementById('plantDescriptionError').textContent = "Plant description must be less than " + limit + " characters";
     } else {
         document.getElementById('plantDescriptionError').textContent = "";
     }
-});
+}
+
+
+document.getElementById('plantDescription').addEventListener('input', description_limit_function);
+window.addEventListener('load', description_limit_function);
