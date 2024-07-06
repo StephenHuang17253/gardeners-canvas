f4dcac9f3f10ef082252b3b4d5cbf36fb2e3b279
lst117 <lst117@uclive.ac.nz>
2024-07-06 21:46:40 +1200

    Hot fix #2 stopping more than one sign being added to number

--- src/main/resources/static/js/gardenSizeFormat.js
+++ src/main/resources/static/js/gardenSizeFormat.js
@@ -1,10 +1,10 @@
 const isNumber = (input) => /^\d+$/.test(input);
 
+const hasSign = (input) => /(\+|\-)/.test(input);
+
 const hasDecimalPoint = (input) => /(\.|,)/.test(input);
 
 const hasExponent = (input) => /[eE]/.test(input);
-
-const isSign = (input) => /(\+|\-)/.test(input);
 
 /**
  * Checks if the key pressed is a number or a decimal point or e and wether it is valid.
@@ -17,7 +17,8 @@ const handleKeyPress = (event) => {
     const value = event.target.value;
     const validDecimalPoint = hasDecimalPoint(pressed) && !hasDecimalPoint(value) && !hasExponent(value)
     const validExponent = hasExponent(pressed) && !hasExponent(value)
-    if (isNumber(pressed) || isSign(pressed) || validDecimalPoint || validExponent) {
+    const validSign = hasSign(pressed) && !hasSign(value);
+    if (isNumber(pressed) || validSign || validDecimalPoint || validExponent) {
         return true;
     } else {
         event.preventDefault();
