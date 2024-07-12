16d958a17f154cfc4e1bb9e2123609ffc365c156
Aditi Sharma <ash313@uclive.ac.nz>
2024-07-12 17:10:28 +1200

    Added rounding so that garden size goes to 2dp once entered

--- src/main/resources/static/js/gardenSizeFormat.js
+++ src/main/resources/static/js/gardenSizeFormat.js
@@ -7,7 +7,7 @@ const hasDecimalPoint = (input) => /(\.|,)/.test(input);
 const hasExponent = (input) => /[eE]/.test(input);
 
 /**
- * Checks if the key pressed is a number or a decimal point or e and wether it is valid.
+ * Checks if the key pressed is a number or a decimal point or e and whether it is valid.
  * If the method returns false, the key press is prevented.
  * @param {Event} event - The keydown event.
  * @returns {boolean} - Returns true if the key is a number or a decimal point, false otherwise.
@@ -26,5 +26,40 @@ const handleKeyPress = (event) => {
     }
 }
 
+
+/**
+ * Handles the blur event of the garden size input field, to limit the final number of decimal places to 2.
+ * @param {Event} event - The blur event object.
+ * @returns {void}
+ */
+const handleBlur = (event) => {
+    const gardenSize = event.target.value;
+    let splitChar = '';
+    if (gardenSize.includes('.')) {
+        splitChar = '.';
+    } else if (gardenSize.includes(',')) {
+        splitChar = ',';
+    }
+
+    let expChar = ''
+    if (gardenSize.includes('E')) {
+        expChar = 'E';
+    } else if (gardenSize.includes('e')) {
+        expChar = 'e';
+    }
+
+    if (splitChar !== '') {
+        const splitInputByDecimal = gardenSize.split(splitChar);
+        if (expChar !== '') {
+            const splitInputbyExp = splitInputByDecimal[1].split(expChar);
+            event.target.value = splitInputByDecimal[0] + splitChar + splitInputbyExp[0].substring(0, 2) + expChar + splitInputbyExp[1];
+        } else {
+            console.log('here');
+            event.target.value = splitInputByDecimal[0] + splitChar + splitInputByDecimal[1].substring(0, 2);
+        }
+    }
+}
+
 document.getElementById('gardenSize').addEventListener('keypress', handleKeyPress);
+document.getElementById('gardenSize').addEventListener('blur', handleBlur);
 
