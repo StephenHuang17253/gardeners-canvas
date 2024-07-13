a977f20546efcc22004da688c8f4c493981fc71d
lst117 <lst117@uclive.ac.nz>
2024-07-13 22:06:04 +1200

    Finished fixing manage friends page

---
+++ src/main/resources/data.sql
@@ -0,0 +1,116 @@
+INSERT INTO USER_TABLE (
+        DATE_OF_BIRTH,
+        VERIFIED,
+        USER_ID,
+        FIRST_NAME,
+        LAST_NAME,
+        PROFILE_PICTURE_FILENAME,
+        EMAIL_ADDRESS,
+        PASSWORD
+    )
+VALUES (
+        '2001-01-01',
+        TRUE,
+        10001,
+        'Sheldon',
+        'Cooper',
+        NULL,
+        'sheldoncooper@email.com',
+        '{bcrypt}$2a$10$NMK7UwogRwxjjMlOqHjF9.8XSdleK4qzjd6tCEksLP3Bp34SnWBoq'
+    ),
+    (
+        '1975-05-01',
+        TRUE,
+        10002,
+        'George',
+        'Cooper',
+        '../../images/user_2_profile_picture.jpg',
+        'georgecoopersr@email.com',
+        '{bcrypt}$2a$10$NMK7UwogRwxjjMlOqHjF9.8XSdleK4qzjd6tCEksLP3Bp34SnWBoq'
+    ),
+    (
+        '1992-11-25',
+        TRUE,
+        10003,
+        'George',
+        'Cooper',
+        '../../images/user_3_profile_picture.jpg',
+        'georgecooperjr@email.com',
+        '{bcrypt}$2a$10$NMK7UwogRwxjjMlOqHjF9.8XSdleK4qzjd6tCEksLP3Bp34SnWBoq'
+    ),
+    (
+        '1945-03-10',
+        TRUE,
+        10004,
+        'Meemaw',
+        '',
+        NULL,
+        'meemawcooper@email.com',
+        '{bcrypt}$2a$10$NMK7UwogRwxjjMlOqHjF9.8XSdleK4qzjd6tCEksLP3Bp34SnWBoq'
+    ),
+    (
+        '2001-01-01',
+        TRUE,
+        10005,
+        'Missy',
+        'Cooper',
+        NULL,
+        'missycooper@email.com',
+        '{bcrypt}$2a$10$NMK7UwogRwxjjMlOqHjF9.8XSdleK4qzjd6tCEksLP3Bp34SnWBoq'
+    ),
+    (
+        '1972-12-12',
+        TRUE,
+        10006,
+        'Mary',
+        'Cooper',
+        NULL,
+        'marycooper@email.com',
+        '{bcrypt}$2a$10$NMK7UwogRwxjjMlOqHjF9.8XSdleK4qzjd6tCEksLP3Bp34SnWBoq'
+    ),
+    (
+        '2001-04-15',
+        TRUE,
+        10007,
+        'Billy',
+        'Sparks',
+        NULL,
+        'billysparks@email.com',
+        '{bcrypt}$2a$10$NMK7UwogRwxjjMlOqHjF9.8XSdleK4qzjd6tCEksLP3Bp34SnWBoq'
+    ),
+    (
+        '1960-08-20',
+        TRUE,
+        10008,
+        'Jeff',
+        'Difford',
+        NULL,
+        'jeffdifford@email.com',
+        '{bcrypt}$2a$10$NMK7UwogRwxjjMlOqHjF9.8XSdleK4qzjd6tCEksLP3Bp34SnWBoq'
+    ),
+    (
+        '1978-03-24',
+        TRUE,
+        10009,
+        'Stuart',
+        'Bloom',
+        NULL,
+        'stuartbloom@email.com',
+        '{bcrypt}$2a$10$NMK7UwogRwxjjMlOqHjF9.8XSdleK4qzjd6tCEksLP3Bp34SnWBoq'
+    );
+INSERT INTO FRIENDSHIP_TABLE (
+        STATUS,
+        FRIEND_SHIP_ID,
+        USER1_USER_ID,
+        USER2_USER_ID
+    )
+VALUES (0, 1001, 10001, 10005),
+    -- pending request from Sheldon to Missy
+    (1, 1002, 10006, 10001),
+    -- Sheldon and Mary friends
+    (0, 1003, 10009, 10001),
+    -- pending request from Stuart to Sheldon
+    (2, 1004, 10007, 10001),
+    -- declined request from Billy to Sheldon
+    (2, 1005, 10001, 10008);
+-- declined request from Sheldon to Jeff
