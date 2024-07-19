6bfc3e6694c796fcde753c35d09a4ecd5cc6233d
Lachlan Stewart <lst117@uclive.ac.nz>
2024-07-19 18:13:52 +1200

    removed unused import

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/HomePageController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/HomePageController.java
@@ -17,8 +17,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
-
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
