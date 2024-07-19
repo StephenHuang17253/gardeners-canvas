52c1c604e44e0958fb5894c8a9e61dce82565f57
Stephen Huang <shu122@uclive.ac.nz>
2024-07-19 19:32:42 +1200

    Extended client-side DoB validation to check min and max ages set by ACs.
    - Previous JS validation only checked date format.
    - Now it checks if the age is below 13, or greater than 120.

--- src/main/resources/static/js/accountFormValidation.js
+++ src/main/resources/static/js/accountFormValidation.js
@@ -8,126 +8,179 @@ const email = document.getElementById("emailAddress")
 const MAX_EMAIL_LENGTH = 320
 let emailJSError = document.getElementById("emailJSError");
 
-
 const dateOfBirth = document.getElementById("dateOfBirth");
 let dateOfBirthJSError = document.getElementById("dateOfBirthJSError");
 
-const handleInvalidFirstName = () => {
-    firstName.setCustomValidity(" ");
-    firstName.classList.add("border-danger");
-    firstNameJSError.style.display = "block";
+const signUpButton = document.querySelector('button[type="submit"]');
+
+/**
+ * Handles the case where the user's input for the first/last name field is invalid.
+ * Sets the border of the input to red, and makes the error message visible.
+ * @param {HTMLElement} inputField - The input field element (firstName or lastName).
+ * @param {HTMLElement} errorField - The error message element (firstNameJSError or lastNameJSError).
+ * @returns {void}
+ */
+const handleInvalidName = (inputField, errorField) => {
+    inputField.setCustomValidity(" "); // ignore underline, is fine (appears because I'm using a param)
+    inputField.classList.add("border-danger");
+    errorField.style.display = "block";
 }
 
-const clearFirstNameError = () => {
-    firstName.setCustomValidity("");
-    firstName.classList.remove("border-danger");
-    firstNameJSError.style.display = "none";
+/**
+ * Clears the error message and removes the red border from the input field.
+ * @param {HTMLElement} inputField - The input field element.
+ * @param {HTMLElement} errorField - The error message element.
+ * @returns {void}
+ */
+const clearNameError = (inputField, errorField) => {
+    inputField.setCustomValidity(""); // ignore underline, is fine (appears because I'm using a param)
+    inputField.classList.remove("border-danger");
+    errorField.style.display = "none";
 }
 
-const handleFirstNameUpdate = (event) => {
-    let firstNameValue = event.target.value
+/**
+ * Handles updates (char input) to the input field for names (first name or last name).
+ * @param {Event} event - The input event.
+ * @param {HTMLElement} errorField - The error message element.
+ * @returns {void}
+ */
+const handleNameUpdate = (event, errorField) => {
+    let nameValue = event.target.value;
     const validNameRegex = /^[A-Za-z\s\-']+$/;
-    console.log(firstNameValue);
-    if (firstNameValue.length > 64) {
-        firstNameJSError.textContent = 'First name must be less than or equal to 64 characters';
-        handleInvalidFirstName();
-    } else if (!validNameRegex.test(firstNameValue)) {
-        firstNameJSError.textContent = 'First name cannot be empty and must only include letters, spaces, hyphens or apostrophes';
-        handleInvalidFirstName();
-    } else if (firstNameValue === "") {
-        firstNameJSError.textContent = 'First name cannot be empty and must only include letters, spaces, hyphens or apostrophes';
-        handleInvalidFirstName();
+    console.log(nameValue);
+    if (nameValue.length > 64) {
+        errorField.textContent = 'Name must be less than or equal to 64 characters';
+        handleInvalidName(event.target, errorField);
+    } else if (!validNameRegex.test(nameValue) || nameValue === "") {
+        errorField.textContent = 'Name cannot be empty and must only include letters, spaces, hyphens or apostrophes';
+        handleInvalidName(event.target, errorField);
     } else {
-        clearFirstNameError();
+        clearNameError(event.target, errorField);
     }
 }
 
-const handleInvalidLastName = () => {
-    lastName.classList.add("border-danger");
-    lastNameJSError.style.display = "block";
-}
-
-const clearLastNameError = () => {
-    lastName.classList.remove("border-danger");
-    lastNameJSError.style.display = "none";
-}
-
-const handleLastNameUpdate = (event) => {
-    let lastNameValue = event.target.value
-    const validNameRegex = /^[A-Za-z\s\-']+$/;
-    console.log(lastNameValue);
-    if (lastNameValue.length > 64) {
-        lastNameJSError.textContent = 'Last name must be less than or equal to 64 characters';
-        handleInvalidLastName();
-    } else if (!validNameRegex.test(lastNameValue)) {
-        lastNameJSError.textContent = 'Last name cannot be empty and must only include letters, spaces, hyphens or apostrophes';
-        handleInvalidLastName();
-    } else if (lastNameValue === "") {
-        lastNameJSError.textContent = 'Last name cannot be empty and must only include letters, spaces, hyphens or apostrophes';
-        handleInvalidLastName();
-    } else {
-        clearLastNameError();
-    }
-}
-
-
-
+/**
+ * Handles the case where the user's input for the email field is invalid.
+ * Sets the border of the input to red, and makes the error message visible.
+ * @returns {void}
+ */
 const handleInvalidEmail = () => {
+    email.setCustomValidity("");
     email.classList.add("border-danger");
     emailJSError.style.display = "block";
 }
 
+/**
+ * Clears the error message and removes the red border from the input field.
+ * @param {HTMLElement} inputField - The input field element.
+ * @param {HTMLElement} errorField - The error message element.
+ * @returns {void}
+ */
 const clearEmailError = () => {
+    email.setCustomValidity("");
     email.classList.remove("border-danger");
     emailJSError.style.display = "none";
 }
 
-const handleEmailInput = (event) => {
-
+/**
+ * Handles updates (char input) to the input field for the user's email.
+ * @param {Event} event - The input event.
+ * @returns {void}
+ */
+const handleEmailUpdate = (event) => {
     let emailValue = event.target.value;
     const emailRegex = /^[\p{L}\p{M}\p{N}]{1,}(?:[._-][\p{L}\p{M}\p{N}]+)*@[a-zA-Z0-9-]{1,}\.[a-zA-Z]{2,}(?:\.[a-zA-Z]{2,})?$/u;
     const [localPart, domainPart] = emailValue.split('@');
 
     if (emailValue.length > MAX_EMAIL_LENGTH) {
-        emailJSError.textContent = "Email is too long, should be 320 characters or less. The local part should be max 64 characters and domain should be max 225 characters"
-        handleInvalidEmail()
+        emailJSError.textContent = "Email is too long, should be 320 characters or less. The local part should be max 64 characters and domain should be max 225 characters";
+        handleInvalidEmail();
     } else if (!emailRegex.test(emailValue)) {
-        emailJSError.textContent = "Email must be in the form 'jane@doe.nz'"
-        handleInvalidEmail()
+        emailJSError.textContent = "Email must be in the form 'jane@doe.nz'";
+        handleInvalidEmail();
     } else if (localPart.length > 64 || domainPart.length > 255) {
-        emailJSError.textContent = "Email is too long, should be 320 characters or less. The local part should be max 64 characters and domain should be max 225 characters"
-        handleInvalidEmail()
+        emailJSError.textContent = "Email is too long, should be 320 characters or less. The local part should be max 64 characters and domain should be max 225 characters";
+        handleInvalidEmail();
     } else {
         clearEmailError();
     }
-
 }
 
-
 /**
- * If a date is invalid then shows an error
+ * Handles the case where the user's input for the date field is invalid.
+ * Sets the border of the input to red, and makes the error message visible.
+ * @returns {void}
  */
 const handleInvalidDate = () => {
     dateOfBirth.setCustomValidity(" ");
-    dateOfBirth.classList.add("border-danger")
+    dateOfBirth.classList.add("border-danger");
     dateOfBirthJSError.style.display = "block";
 }
 
 /**
- * Removes invalid date error when a user edits it
+ * Clears the error message and removes the red border from the input field.
+ * @param {HTMLElement} inputField - The input field element.
+ * @param {HTMLElement} errorField - The error message element.
+ * @returns {void}
  */
 const clearDateError = () => {
     dateOfBirth.setCustomValidity("");
     dateOfBirthJSError.style.display = "none";
-    dateOfBirth.classList.remove("border-danger")
+    dateOfBirth.classList.remove("border-danger");
     document.getElementById("dateOfBirthError").textContent = "";
 }
 
+/**
+ * Handles updates to the input field for date of birth.
+ * @param {Event} event - The input event.
+ * @returns {void}
+ */
+const handleDateUpdate = (event) => {
+    const dateValue = new Date(event.target.value);
+    const today = new Date();
+    const age = today.getFullYear() - dateValue.getFullYear();
+    const monthDiff = today.getMonth() - dateValue.getMonth();
+    const dayDiff = today.getDate() - dateValue.getDate();
+
+    let validAge = false;
+
+    if (age > 120 || (age === 120 && (monthDiff > 0 || (monthDiff === 0 && dayDiff >= 0)))) {
+        dateOfBirthJSError.textContent = "Age cannot be more than 120 years";
+    } else if (age < 13 || (age === 13 && (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)))) {
+        dateOfBirthJSError.textContent = "You must be at least 13 years old";
+    } else {
+        validAge = true;
+    }
+
+    if (!dateOfBirth.checkValidity() || !validAge) {
+        handleInvalidDate();
+    } else {
+        clearDateError();
+    }
+}
+
+/**
+ * Validates the form on the client-side when the user presses the Submit button.
+ * If there is an invalid input, prevent the submission of the form.
+ * @param {Event} event - The input event.
+ * @returns {void}
+ */
+const handleFormSubmit = (event) => {
+    handleNameUpdate({ target: firstName }, firstNameJSError);
+    handleNameUpdate({ target: lastName }, lastNameJSError);
+    handleEmailUpdate({ target: email });
+    handleDateUpdate({ target: dateOfBirth });
+
+    // Prevent form submission if there are any validation errors
+    if (!firstName.checkValidity() || !lastName.checkValidity() || !email.checkValidity() || !dateOfBirth.checkValidity()) {
+        event.preventDefault();
+    }
+}
+
 // Event Listeners
-firstName.addEventListener("input", handleFirstNameUpdate);
-lastName.addEventListener("input", handleLastNameUpdate);
-email.addEventListener("input", handleEmailInput);
+firstName.addEventListener("input", (event) => handleNameUpdate(event, firstNameJSError));
+lastName.addEventListener("input", (event) => handleNameUpdate(event, lastNameJSError));
+email.addEventListener("input", handleEmailUpdate);
+dateOfBirth.addEventListener("input", handleDateUpdate);
 
-
-
-
+signUpButton.addEventListener('click', handleFormSubmit);
