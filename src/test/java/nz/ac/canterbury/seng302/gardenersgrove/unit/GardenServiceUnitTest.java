dc027c1f3f5bb401a2308b6090651390297c10ec
eda60 <eda60@uclive.ac.nz>
2024-07-22 13:10:41 +1200

    Test not working.
    Began implementing unit tests for the garden service function for updating the garden coordinates. Test is not completed.

---
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/unit/GardenServiceUnitTest.java
@@ -0,0 +1,93 @@
+package nz.ac.canterbury.seng302.gardenersgrove.unit;
+
+import nz.ac.canterbury.seng302.gardenersgrove.entity.User;
+import nz.ac.canterbury.seng302.gardenersgrove.repository.GardenRepository;
+import nz.ac.canterbury.seng302.gardenersgrove.service.GardenService;
+import nz.ac.canterbury.seng302.gardenersgrove.service.UserService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import nz.ac.canterbury.seng302.gardenersgrove.entity.Garden;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+public class GardenServiceUnitTest {
+
+    @Mock
+    GardenRepository gardenRepository;
+
+    @Mock
+    private UserService userService;
+
+    static GardenService gardenService;
+
+    private Garden garden;
+
+    private int gardenId;
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy").withLocale(Locale.ENGLISH);
+    LocalDate date = LocalDate.parse("01/01/2001", formatter);
+
+
+    @BeforeEach
+    void setup() {
+         gardenRepository = Mockito.mock(GardenRepository.class);
+         userService = Mockito.mock(UserService.class);
+         gardenService = new GardenService(gardenRepository, userService);
+
+        User testUser1 = Mockito.spy(new User("John", "Doe", "jhonDoe@ManageFriendsControllerIntegrationTest.com", LocalDate.of(2003,5,2)));
+
+        Mockito.when(testUser1.getId()).thenReturn(1L);
+        Mockito.when(userService.getUserById(1L)).thenReturn(testUser1);
+
+
+        garden = new Garden(
+                "test",
+                "test",
+                "test",
+                "test",
+                "test",
+                "80",
+                "test",
+                10.0,
+                false,
+                "",
+                "",
+                testUser1);
+
+        gardenService.addGarden(garden);
+
+
+    }
+
+
+
+    @Test
+    void testUpdateGardenLocationCoordinates_Success() {
+
+        Garden updatedGarden = gardenService.updateGardenCoordinates(garden.getGardenId(), "1.1", "1.1");
+        assertEquals("1.1", updatedGarden.getGardenLatitude());
+        assertEquals("1.1", updatedGarden.getGardenLongitude());
+    }
+
+    @Test
+    void testUpdateGardenLocationCoordinates_InvalidId() {
+        Mockito.when(gardenService.getGardenById(2L)).thenReturn(Optional.empty());
+        Exception error = assertThrows(IllegalArgumentException.class,() -> {gardenService.updateGardenCoordinates(2L, "1.1", "1.1");});
+        assertEquals("Invalid garden ID", error.getMessage());
+        assertEquals("", garden.getGardenLongitude());
+        assertEquals("", garden.getGardenLatitude());
+
+    }
+}
