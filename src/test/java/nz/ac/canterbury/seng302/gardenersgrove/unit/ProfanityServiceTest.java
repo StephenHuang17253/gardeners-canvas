eaa8acec4ff491827ada571d98f5378ca5b904ea
Dominic Gorny <dgo65@uclive.ac.nz>
2024-07-24 20:22:25 +1200

    Test: Added automated test for profanity service that ensures rate limiting does not allow two calls to call withing less than 1.1 seconds of one another.

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/unit/ProfanityServiceTest.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/unit/ProfanityServiceTest.java
@@ -2,14 +2,19 @@ package nz.ac.canterbury.seng302.gardenersgrove.unit;
 
 import nz.ac.canterbury.seng302.gardenersgrove.service.ProfanityService;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.util.Assert;
 
 import java.io.IOException;
 import java.net.http.HttpClient;
 import java.net.http.HttpResponse;
+import java.util.Date;
+
+import static org.mockito.Mockito.when;
 
 class ProfanityServiceTest {
 
@@ -52,11 +57,20 @@ class ProfanityServiceTest {
         Mockito.when(httpClientMock.send(Mockito.any(), Mockito.eq(HttpResponse.BodyHandlers.ofString()))).thenReturn(mockHttpResponse);
         String profanity_sentence = "Hello there John Doe";
         Assertions.assertTrue(profanityService.containsProfanity(profanity_sentence));
-
     }
 
+    @Test
+    void containsProfanityCall_BackToBackCalls_CallsRateLimited() throws IOException, InterruptedException {
+        HttpResponse<String> mockHttpResponse = Mockito.mock(HttpResponse.class);
+        Mockito.when(mockHttpResponse.body()).thenReturn("{\"OriginalText\":\"No bad input\",\"NormalizedText\":\" bad input\",\"Misrepresentation\":null,\"Language\":\"eng\",\"Terms\":[{\"Index\":7,\"OriginalIndex\":12,\"ListId\":0,\"Term\":\"BadWord\"}],\"Status\":{\"Code\":3000,\"Description\":\"OK\",\"Exception\":null},\"TrackingId\":\"e7b5c1ba-48cf-4b58-b3f1-41dce34ae0c5\"}");
+        Mockito.when(httpClientMock.send(Mockito.any(), Mockito.eq(HttpResponse.BodyHandlers.ofString()))).thenReturn(mockHttpResponse);
+        String profanity_sentence = "Hello there John Doe";
+        profanityService.moderateContent(profanity_sentence);
+        long preSecondCallTime = new Date().getTime();
+        profanityService.moderateContent(profanity_sentence);
+        long postSecondCallTime = new Date().getTime();
 
+        Assertions.assertTrue((preSecondCallTime + 1100L) < postSecondCallTime);
 
-
-
+    }
 }
