0580b78ecd05afde2f5e11778ee767cc75af5ad5
Luke Edwards <led46@uclive.ac.nz>
2024-07-27 17:09:37 +1200

    Fix - merge conflicts

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/GardensController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/GardensController.java
@@ -41,24 +41,11 @@ import nz.ac.canterbury.seng302.gardenersgrove.util.FriendshipStatus;
 import nz.ac.canterbury.seng302.gardenersgrove.validation.ValidationResult;
 import nz.ac.canterbury.seng302.gardenersgrove.validation.fileValidation.FileType;
 import nz.ac.canterbury.seng302.gardenersgrove.validation.fileValidation.FileValidator;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.concurrent.Semaphore;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.servlet.support.RequestContextUtils;
 
