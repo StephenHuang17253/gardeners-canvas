58feb04b59875fa0a3f183ab04a000fa6c744343
Stephen Huang <shu122@uclive.ac.nz>
2024-07-30 17:39:34 +1200

    Add PlantInfoModel which is displayed on PlantWikiDetailPage when clicking on a search result.
    - It is now possible to:
    search a plant name to get a page of results.
    - Then you can click on those results to access detailed information for that plant.

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/PlantWikiController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/PlantWikiController.java
@@ -1,6 +1,7 @@
 package nz.ac.canterbury.seng302.gardenersgrove.controller;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import nz.ac.canterbury.seng302.gardenersgrove.model.PlantInfoModel;
 import nz.ac.canterbury.seng302.gardenersgrove.service.PlantInfoService;
 import nz.ac.canterbury.seng302.gardenersgrove.service.SecurityService;
 import org.slf4j.Logger;
@@ -9,12 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 
 /**
@@ -33,8 +32,6 @@ public class PlantWikiController {
         this.plantInfoService = plantInfoService;
     }
 
-
-
     private void displayPlantDetails(Model model, JsonNode plantData) {
             String plantName = plantData.get("common_name").asText();
             String plantImageUrl = plantData.get("default_image").get("original_url").asText();
@@ -44,36 +41,55 @@ public class PlantWikiController {
             logger.info(plantImageUrl);
     }
 
-    private void addSearchSuggestions(Model model, JsonNode plantList) {
-
-        JsonNode dataArray = plantList.path("data");
-
-        // Ensure dataArray is an array node
-        if (dataArray.isArray()) {
-            // Convert JsonNode to a stream and extract common names
-            List<String> commonNames = StreamSupport.stream(dataArray.spliterator(), false)
-                    .map(node -> node.path("common_name").asText())
-                    .collect(Collectors.toList());
-
-            // Add the common names to the model
-            model.addAttribute("searchSuggestions", commonNames);
-        }
-    }
-
+    /**
+     * This method creates the get mapping for the plant wiki page where users can search for plant information.
+     * @param search the query string
+     * @param model the model
+     * @return template for the plant wiki page
+     * @throws IOException          If an I/O error occurs while making the
+     *                              requesting
+     * @throws InterruptedException If an interruption occurs while waiting for
+     *                              response
+     */
     @GetMapping("/plant-wiki")
-    public String viewPlantWiki(@RequestParam(name = "search", required = false) String search, Model model) throws IOException, InterruptedException {
-        logger.info("GET /plant-wiki with search: " + search);
-        model.addAttribute("loggedIn", securityService.isLoggedIn());
+    public String viewPlantWiki(@RequestParam(name = "search", required = false)
+                                String search,
+                                Model model) throws IOException, InterruptedException {
+        logger.info("GET /plant-wiki with search: {}", search);
 
         if (search != null && !search.isEmpty()) {
             model.addAttribute("searchTerm", search);
-            JsonNode plantList = plantInfoService.getPlantListJson(search);
+            JsonNode plantList = plantInfoService.getPlantListJson(search, true);
 
             model.addAttribute("plantList", plantList.get("data"));
 
         }
 
         return "plantWikiPage";
+    }
+
+    /**
+     * This method creates the get mapping for the plant details page which displays information for a certain plant.
+     * @param plantId               id of the plant
+     * @param model                 the model
+     * @return                      template for the plant details pge
+     * @throws IOException          If an error occurs while making request
+     * @throws InterruptedException If request is interrupted
+     */
+    @GetMapping("/plant-wiki/{plantId}/details")
+    public String viewPlantDetails(@PathVariable Long plantId,
+                                   Model model) throws IOException, InterruptedException {
+        logger.info("GET /plant-wiki/{}/details", plantId);
+
+
+        JsonNode plantDetails = plantInfoService.getPlantDetailsJson(String.valueOf(plantId), true);
+
+        PlantInfoModel plant = new PlantInfoModel(plantDetails);
+
+        model.addAttribute("plant", plant);
+
+
+        return "plantWikiDetailPage";
     }
 
 }
