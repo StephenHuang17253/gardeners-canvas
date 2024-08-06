8ed68d5ea0eebf7411cf61579212cd57cf8ca8ba
Tyler <tfa46@uclive.ac.nz>
2024-08-06 23:14:19 +1200

    Hotfix: allow cross-origin requests from plant wiki

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/PlantWikiController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/PlantWikiController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -116,6 +117,7 @@ public class PlantWikiController {
      * @return template for the plant wiki page
      */
     @GetMapping("/plant-wiki")
+    @CrossOrigin
     public String viewPlantWiki(@RequestParam(name = "search", required = false)
                                 String search,
                                 Model model) {
