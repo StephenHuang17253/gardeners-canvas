8f6a893dc4d2da6c46d41471461b41e9de885b7f
Dominic Gorny <dgo65@uclive.ac.nz>
2024-08-15 15:14:24 +1200

    Created CacheableWeatherResponseData object to allow for weather response Caching

---
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/component/CacheableWeatherResponseData.java
@@ -0,0 +1,47 @@
+package nz.ac.canterbury.seng302.gardenersgrove.component;
+
+import java.util.Date;
+
+public class CacheableWeatherResponseData {
+
+    private String locationIdentifier;
+    private WeatherResponseData weatherResponseData;
+    private Long creationTime;
+
+    public CacheableWeatherResponseData(String locationId, WeatherResponseData weatherData)
+    {
+        this.weatherResponseData = weatherData;
+        this.locationIdentifier = locationId;
+        this.creationTime = new Date().getTime();
+    }
+
+    public String getLocationIdentifier() {
+        return locationIdentifier;
+    }
+
+    public void setLocationIdentifier(String locationIdentifier) {
+        this.locationIdentifier = locationIdentifier;
+    }
+
+    public WeatherResponseData getWeatherResponseData() {
+        return weatherResponseData;
+    }
+
+    public void setWeatherResponseData(WeatherResponseData weatherResponseData) {
+        this.weatherResponseData = weatherResponseData;
+    }
+
+    public Long getCreationTime() {
+        return creationTime;
+    }
+
+    public void setCreationTime(Long creationTime) {
+        this.creationTime = creationTime;
+    }
+
+    public boolean isExpired()
+    {
+        return (new Date().getTime() - this.creationTime) > 10000;
+
+    }
+}
