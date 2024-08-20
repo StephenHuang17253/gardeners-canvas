f93bc88385b0c81f4cb01f66221eb89fba0b40ff
Tyler <tfa46@uclive.ac.nz>
2024-08-20 13:18:59 +1200

    Created Unit Tests for getImageFileType in FileService type

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/integration/FileServiceTest.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/integration/FileServiceTest.java
@@ -222,4 +222,47 @@ class FileServiceTest {
         });
         assertEquals("Could not delete the file", exception.getMessage());
     }
+
+    @Test
+    public void testFileType_FileIsSVG_ReturnsSVG() {
+        assertEquals("image/svg+xml", fileService.getImageFileType("example.svg"));
+        assertEquals("image/svg+xml", fileService.getImageFileType("example.SVG"));
+    }
+
+    @Test
+    void testFileType_FileIsPNG_ReturnsPNG() {
+        assertEquals("image/png", fileService.getImageFileType("example.png"));
+        assertEquals("image/png", fileService.getImageFileType("example.PNG"));
+    }
+
+    @Test
+    void testFileType_FileIsJPG_ReturnsJPEG() {
+        assertEquals("image/jpeg", fileService.getImageFileType("example.jpg"));
+        assertEquals("image/jpeg", fileService.getImageFileType("example.JPG"));
+    }
+
+    @Test
+    void testFileType_FileIsJPEG_ReturnsJPEG() {
+        assertEquals("image/jpeg", fileService.getImageFileType("example.jpeg"));
+        assertEquals("image/jpeg", fileService.getImageFileType("example.JPEG"));
+    }
+
+    @Test
+    void testFileType_FileIsInvalid_ReturnsUnknown() {
+        assertEquals("unknown", fileService.getImageFileType("example.txt"));
+        assertEquals("unknown", fileService.getImageFileType("example.docx"));
+        assertEquals("unknown", fileService.getImageFileType("example"));
+        assertEquals("unknown", fileService.getImageFileType("SVGInName.butNotExtension"));
+        assertEquals("unknown", fileService.getImageFileType("PNGInName.butNotExtension"));
+        assertEquals("unknown", fileService.getImageFileType("JPGInName.butNotExtension"));
+        assertEquals("unknown", fileService.getImageFileType("JPEGInName.butNotExtension"));
+        assertEquals("unknown", fileService.getImageFileType("SVGNotFullExtension.SV"));
+        assertEquals("unknown", fileService.getImageFileType("SVGNotFullExtension.VG"));
+        assertEquals("unknown", fileService.getImageFileType("PNGNotFullExtension.P"));
+        assertEquals("unknown", fileService.getImageFileType("PNGNotFullExtension.PN"));
+        assertEquals("unknown", fileService.getImageFileType("PNGNotFullExtension.NG"));
+        assertEquals("unknown", fileService.getImageFileType("JPGNotFullExtension.JP"));
+        assertEquals("unknown", fileService.getImageFileType("JPGNotFullExtension.PG"));
+        assertEquals("unknown", fileService.getImageFileType("JPEGNotFullExtension.PEG"));
+    }
 }
