96502b5ce3d44d220ee8e15b7504140f9bebfe71
Aditi Sharma <ash313@uclive.ac.nz>
2024-08-20 13:04:17 +1200

    removed modal and formatted tag box to be aligned in the center and the errors to not be wrapped.

--- src/main/resources/static/js/tagModalOpener.js
+++
@@ -1,8 +0,0 @@
-const openTagModal = document.getElementById("openTagModalButton");
-const openModal = document.getElementById("openModal")
-
-window.addEventListener('DOMContentLoaded', () => {
-    if (openModal !== null) {
-        openTagModal.click();
-    }
-})
