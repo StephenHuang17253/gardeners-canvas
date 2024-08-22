9e5e9526fea6f5208211f55bc1977022c4d79bcb
Tyler <tfa46@uclive.ac.nz>
2024-08-23 11:57:42 +1200

    Moved the 2d grid view into the plant details 2d page, conencted to button

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/TwoDGardenController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/TwoDGardenController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class twoDGardenController {
+public class TwoDGardenController {
 
     @GetMapping("/2d")
     public String twoDGardenPlan() {
