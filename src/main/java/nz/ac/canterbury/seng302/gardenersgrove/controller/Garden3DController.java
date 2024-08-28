ebb869804716d4723c12689d96018fe3d2543897
Stephen Huang <shu122@uclive.ac.nz>
2024-08-29 11:49:34 +1200

    Prevent opening 3D view for private gardens for non-friends

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/Garden3DController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/Garden3DController.java
@@ -2,8 +2,12 @@ package nz.ac.canterbury.seng302.gardenersgrove.controller;
 
 import jakarta.servlet.http.HttpServletResponse;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.Garden;
+import nz.ac.canterbury.seng302.gardenersgrove.entity.User;
 import nz.ac.canterbury.seng302.gardenersgrove.model.GardenDetailModel;
+import nz.ac.canterbury.seng302.gardenersgrove.service.FriendshipService;
 import nz.ac.canterbury.seng302.gardenersgrove.service.GardenService;
+import nz.ac.canterbury.seng302.gardenersgrove.service.SecurityService;
+import nz.ac.canterbury.seng302.gardenersgrove.util.FriendshipStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +16,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.Objects;
 import java.util.Optional;
 
 @Controller
@@ -20,11 +25,14 @@ public class Garden3DController {
     Logger logger = LoggerFactory.getLogger(Garden3DController.class);
 
     private final GardenService gardenService;
+    private final SecurityService securityService;
+    private final FriendshipService friendshipService;
 
     @Autowired
-    public Garden3DController(GardenService gardenService){
+    public Garden3DController(GardenService gardenService, SecurityService securityService, FriendshipService friendshipService){
         this.gardenService = gardenService;
-
+        this.securityService = securityService;
+        this.friendshipService = friendshipService;
     }
 
     @GetMapping("/3D-garden/{gardenId}")
@@ -38,6 +46,32 @@ public class Garden3DController {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
             return "404";
         }
+
+        Garden garden = optionalGarden.get();
+        User currentUser = securityService.getCurrentUser();
+        User gardenOwner = garden.getOwner();
+
+        if (!garden.getIsPublic() ) {
+            FriendshipStatus userOwnerRelationship;
+            if(Objects.equals(gardenOwner.getId(), currentUser.getId()))
+            {
+                userOwnerRelationship = FriendshipStatus.ACCEPTED;
+            }
+            else
+            {
+                userOwnerRelationship = friendshipService.checkFriendshipStatus(gardenOwner,currentUser);
+            }
+
+            if (userOwnerRelationship != FriendshipStatus.ACCEPTED)
+            {
+                response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                model.addAttribute("message", "This isn't your patch of soil. No peeking at the neighbor's garden without an invite!");
+                return "403";
+            }
+
+        }
+
+
         model.addAttribute("garden", new GardenDetailModel(optionalGarden.get()));
         return "garden3DPage";
     }
