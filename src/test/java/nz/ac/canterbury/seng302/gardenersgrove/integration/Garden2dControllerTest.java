fdf698aab73d1f36cf48bd557533bae86c570b30
Aditi Sharma <ash313@uclive.ac.nz>
2024-09-03 18:21:15 +1200

    added test for missed code in save2DGarden endpoint (noticed from sonarqube)

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/integration/Garden2dControllerTest.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/integration/Garden2dControllerTest.java
@@ -296,5 +296,21 @@ public class Garden2dControllerTest {
 
     }
 
+    @Test
+    @WithMockUser(username = "janeDoe@email.com")
+    void save2DGarden_UserNotAuthorizedAndGardenDoesNotExist_Return404() throws Exception {
+        gridItemLocationRepository.deleteAll();
+        Long gardenId = userService.getUserByEmail("jhonDoe@Garden2dControllerTest.com").getGardens().get(0).getGardenId();
+        List<String> idList = new ArrayList<>();
+        List<Double> xCoordList = new ArrayList<>();
+        List<Double> yCoordList = new ArrayList<>();
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/2D-garden/" + gardenId + "/save").with(csrf())
+                        .param("idList", JSONArray.toJSONString(idList))
+                        .param("xCoordList", JSONArray.toJSONString(xCoordList))
+                        .param("yCoordList", JSONArray.toJSONString(yCoordList)))
+                .andExpect(MockMvcResultMatchers.status().isForbidden());
+    }
+
 
 }
