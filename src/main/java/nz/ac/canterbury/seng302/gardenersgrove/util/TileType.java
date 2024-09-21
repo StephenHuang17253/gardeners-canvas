77fea5231bf09ea5257bdf2152fbb694d342e7ef
Dominic Gorny <dgo65@uclive.ac.nz>
2024-09-21 18:44:27 +1200

    Feature: Added Tile types to store different texture selections

---
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/util/TileType.java
@@ -0,0 +1,10 @@
+package nz.ac.canterbury.seng302.gardenersgrove.util;
+
+/**
+ * represents types of tile imagery that can be used to represent user gardens in 2 or 3 D
+ */
+public enum TileType {
+    GRASS,
+    CONCRETE,
+    STONE
+}
