6a7f61c5aad0b50784257f3899eac22c4e134a6d
Aditi Sharma <ash313@uclive.ac.nz>
2024-09-24 16:20:55 +1200

    added in cucumber test

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/cucumber/step_definitions/U5006_GardenDecorations.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/cucumber/step_definitions/U5006_GardenDecorations.java
@@ -1,26 +1,98 @@
 package nz.ac.canterbury.seng302.gardenersgrove.cucumber.step_definitions;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import nz.ac.canterbury.seng302.gardenersgrove.controller.Garden3DController;
+import nz.ac.canterbury.seng302.gardenersgrove.entity.Decoration;
+import nz.ac.canterbury.seng302.gardenersgrove.entity.Garden;
+import nz.ac.canterbury.seng302.gardenersgrove.entity.GridItemLocation;
+import nz.ac.canterbury.seng302.gardenersgrove.entity.User;
+import nz.ac.canterbury.seng302.gardenersgrove.service.DecorationService;
+import nz.ac.canterbury.seng302.gardenersgrove.service.GridItemLocationService;
+import nz.ac.canterbury.seng302.gardenersgrove.service.UserService;
+import nz.ac.canterbury.seng302.gardenersgrove.util.DecorationCategory;
+import nz.ac.canterbury.seng302.gardenersgrove.util.GridItemType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.io.UnsupportedEncodingException;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
 public class U5006_GardenDecorations {
-    @Given("There is a garden decoration {string} saved in my garden {string}")
-    public void there_is_a_garden_decoration_saved_in_my_garden(String decorationName, String gardenName) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private DecorationService decorationService;
+
+    @Autowired
+    private GridItemLocationService gridItemLocationService;
+
+    @Autowired
+    private Garden3DController garden3DController;
+
+    MockMvc mockMvc;
+
+    MvcResult mvcResult;
+
+    @Autowired
+    public ObjectMapper objectMapper;
+
+
+    @Before
+    public void beforeOrAfterAll() {
+        mockMvc = MockMvcBuilders.standaloneSetup(garden3DController).build();
     }
 
-    @When("I open the 3D view")
-    public void i_open_the_3d_view() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    @Given("There is a garden decoration {string} saved in my {string} garden {string}")
+    public void there_is_a_garden_decoration_saved_in_my_garden(String decorationCategory, String userEmail, String gardenName) {
+        User user = userService.getUserByEmail(userEmail);
+        Optional<Garden> matchingGarden = user.getGardens().stream()
+                .filter(garden -> garden.getGardenName().equals(gardenName))
+                .findFirst();
+        Garden garden = matchingGarden.get();
+        Decoration decoration1 = decorationService.addDecoration(new Decoration(garden, DecorationCategory.valueOf(decorationCategory.toUpperCase())));
+        gridItemLocationService.addGridItemLocation(new GridItemLocation(decoration1.getId(), GridItemType.DECORATION, garden, 6, 6));
     }
 
-    @Then("I can see the garden decoration {string}")
-    public void i_can_see_the_garden_decoration(String decorationName) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    @When("I {string} open the 3D view of my garden {string}")
+    public void i_open_the_3d_view_of_my_garden(String userEmail, String gardenName) throws Exception {
+
+        User user = userService.getUserByEmail(userEmail);
+        Optional<Garden> matchingGarden = user.getGardens().stream()
+                .filter(garden -> garden.getGardenName().equals(gardenName))
+                .findFirst();
+        Garden garden = matchingGarden.get();
+        mvcResult = mockMvc
+                .perform(get("/3D-garden-layout/{gardenId}", garden.getGardenId()))
+                .andExpect(status().isOk())
+                .andReturn();
     }
+
+    @Then("I can see the garden decoration {string} in my {string} garden {string}")
+    public void i_can_see_the_garden_decoration_in_my_garden(String decorationCategory, String userEmail, String gardenName) throws UnsupportedEncodingException, JsonProcessingException {
+        JsonNode jsonNode = objectMapper.readTree(mvcResult.getResponse().getContentAsString());
+        JsonNode item = jsonNode.get(0);
+        //should match decoration1 in before_all
+        assertEquals(item.get("xcoordinate").asInt(), 6);
+        assertEquals(item.get("ycoordinate").asInt(), 6);
+        assertEquals(item.get("category").asText(), decorationCategory.substring(0, 1).toUpperCase() + decorationCategory.substring(1));
+        assertEquals(item.get("type").asText(), "DECORATION");
+    }
+
 
 }
--- src/test/resources/features/u5006-garden-decorations.feature
+++ src/test/resources/features/u5006-garden-decorations.feature
@@ -6,6 +6,6 @@ Feature: As Liam, I want to be able to add decorations such as fountains and gno
     And User "oliver@email.com" has a garden "oliver's Garden" located in "Christchurch", "New Zealand"
 
   Scenario: AC3 - Given I have placed a decoration and saved it, when I view the garden in 3D, then I can see it in the garden.
-    Given There is a garden decoration "fountain" saved in my garden "oliver's Garden"
-    When I open the 3D view
-    Then I can see the garden decoration "fountain"
+    Given There is a garden decoration "fountain" saved in my "oliver@email.com" garden "oliver's Garden"
+    When I "oliver@email.com" open the 3D view of my garden "oliver's Garden"
+    Then I can see the garden decoration "fountain" in my "oliver@email.com" garden "oliver's Garden"
