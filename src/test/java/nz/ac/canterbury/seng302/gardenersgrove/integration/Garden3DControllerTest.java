f7ca79ab1a603c00959fd51f3a25566e2d435fe1
Aditi Sharma <ash313@uclive.ac.nz>
2024-09-26 06:49:28 +1200

    removed unused import

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/integration/Garden3DControllerTest.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/integration/Garden3DControllerTest.java
@@ -6,7 +6,6 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 
 import com.fasterxml.jackson.core.JsonProcessingException;
